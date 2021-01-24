710cd08fdfdbdf05b0b3312de2271e3bd27fbeeb
PatOn <mahomed2278@gmail.com>
2021-01-24 08:47:51 +0000

    Mandatory - done

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,25 +1,74 @@
-function setAlarm() {}
+//objects UI
+const inputTime = document.getElementById('alarmSet');
+const btnTimeSet = document.getElementById('set');
+const btnTimeStop = document.getElementById('stop');
+const alarmDisplay = document.getElementById('timeRemaining');
+const body = document.getElementById('main');
+//Variable made to global scope
+let timer;
+let flashesInterval;
+const audio = new Audio("alarmsound.mp3");
 
-// DO NOT EDIT BELOW HERE
-
-var audio = new Audio("alarmsound.mp3");
-
-function setup() {
-  document.getElementById("set").addEventListener("click", () => {
-    setAlarm();
-  });
-
-  document.getElementById("stop").addEventListener("click", () => {
-    pauseAlarm();
-  });
-}
-
+//functions
 function playAlarm() {
   audio.play();
+  flashingBackground()
 }
 
-function pauseAlarm() {
+function stopAlarm() {
+  clearInterval(timer);
+  clearInterval(flashesInterval);
   audio.pause();
+  alarmDisplay.textContent = `Time Remaining: 00: 00`;
+  body.style.backgroundColor = 'white';
 }
 
-window.onload = setup;
+//let use closures 
+function flashingBackground() {
+  let noOfChanges = 11;
+  function flashes() {
+    body.style.backgroundColor = noOfChanges % 2 === 0 ? "red" : "white";
+    if (noOfChanges === 0) clearInterval(flashesInterval)
+    noOfChanges--
+  };
+
+  flashes()
+  flashesInterval = setInterval(flashes, 1000);
+  return flashesInterval
+}
+
+const clockCounting = function () {
+  let time = inputTime.value;
+  body.style.backgroundColor = 'white'; // reset because of the bug
+  const tickTack = function () {
+    const min = String(Math.trunc(time / 60)).padStart(2, 0);
+    const sec = String(time % 60).padStart(2, 0);
+
+    //Time Update on UI
+    alarmDisplay.textContent = `Time Remaining: ${min}: ${sec} `;
+    if (time === 0) {
+      clearInterval(timer)
+      playAlarm();
+      // TODO add here another function to start alarm and stop counting
+    }
+
+    //decrease by 1 second
+    time--
+  };
+  tickTack()
+  timer = setInterval(tickTack, 1000);
+  return timer;
+}
+
+//adding events
+btnTimeSet.addEventListener("click", () => {
+  clockCounting();
+  inputTime.value = 0;
+
+});
+
+btnTimeStop.addEventListener("click", () => {
+  stopAlarm();
+
+});
+// window.onload = setup;
