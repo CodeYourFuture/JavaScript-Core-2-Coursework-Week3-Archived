e0f1d3c190747e50da621f32365e293c56480100
Know-Thyself <67852769+Know-Thyself@users.noreply.github.com>
2021-01-25 04:53:44 +0000

    The alarm is now fully functional.
    I had to move the script tag down to the bottom of the html file in order to make it work.

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,21 @@
-function setAlarm() {}
+let input = document.getElementById("alarmSet");
+let timeRemaining = document.getElementById("timeRemaining");
+var randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+function setAlarm() {
+  if (input.value > 0) {
+    input.value = input.value - 1;
+    let time = input.value;
+    let minutes = Math.floor(time/60);
+    let seconds = time % 60;
+    seconds = seconds < 10 ? '0' + seconds:seconds;
+    minutes = minutes < 10 ? '0' + minutes:minutes;
+    timeRemaining.innerHTML = `Time Remaining: ${minutes}:${seconds}`;
+  } else if (timeRemaining.innerHTML === 'Time Remaining: ' + '00:' + '00') {
+      audio.play();
+      document.body.style.backgroundColor = '#' + randomColor;
+    }
+}
 
 // DO NOT EDIT BELOW HERE
 
@@ -6,11 +23,13 @@ var audio = new Audio("alarmsound.mp3");
 
 function setup() {
   document.getElementById("set").addEventListener("click", () => {
+    let countdown = setInterval(setAlarm, 1000);
     setAlarm();
-  });
 
-  document.getElementById("stop").addEventListener("click", () => {
+  document.getElementById("stop").addEventListener("click", () => { 
+    clearInterval(countdown, 10);
     pauseAlarm();
+  });
   });
 }
 
