89855dcbbbb122f43dfd30206fe600a91e8601a8
Kos0606 <65927936+Kos0606@users.noreply.github.com>
2021-01-25 15:05:20 +0000

    Javascript-Core2-Coursework-week3

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,25 @@
-function setAlarm() {}
+/*# Make an Alarm Clock
+
+In this folder you will find the basic setup of an alarm clock.
+
+You should only change code in the `alarmclock.js` file.
+
+## How the clock should work
+
+When you click the `Set Alarm` button the counter at the top of the screen should change to the number you entered in the `input` field. For example, if the `input` field says `10` then the title should say `Time Remaining: 00:10`.
+
+Every one second the title should count down by one.
+
+When the `Time Remaining` reaches `00:00` the alarm should play a sound. You can make the sound happen by using `playAlarm()`.
+
+You can stop the alarm sound by pressing the `Stop Alarm` button.*/
+function setAlarm() {
+  var clickAlarmBtn = document.getElementById("set");
+  clickAlarmBtn.addEventListener("click", function() {
+    var inputNumber = document.getElementById("alarmSet").type;
+    
+  })
+}
 
 // DO NOT EDIT BELOW HERE
 
