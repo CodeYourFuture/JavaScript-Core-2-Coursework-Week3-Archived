b54c98e8723fcc8a4c9e353e1a74732042ab230c
nirmeet-baweja <73722230+nirmeet-baweja@users.noreply.github.com>
2021-01-27 18:44:11 +0000

    v1 - Alarm Clock

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,35 @@
-function setAlarm() {}
+let isAlarmSet = false;
+
+function setBtnEventListener() {
+  let heading = document.getElementById("timeRemaining");
+  let timer;
+  let input = document.getElementById("alarmSet");
+  if(input.value !== "" && !isAlarmSet) {
+    console.log("Alarm has been set!");
+    isAlarmSet = true;
+    timer = input.value;
+    input.value = "";
+  }
+  let intervalId = setInterval(function() {
+    if(timer >= 0) {
+      let minutes = Math.floor(timer / 60);
+      let seconds = timer % 60;
+      timer--;
+      console.log("Timer decremented!");
+      heading.innerHTML = `Time Remaining: ${minutes.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false })}:${seconds.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false })}`;
+    }
+    if(timer === -1) {
+      playAlarm();
+      clearInterval(intervalId);
+      isAlarmSet = false;
+    }
+  }, 1000);
+}
+
+function setAlarm() {
+  let setBtn = document.getElementById("set");
+  setBtn.addEventListener("click", setBtnEventListener);
+}
 
 // DO NOT EDIT BELOW HERE
 
