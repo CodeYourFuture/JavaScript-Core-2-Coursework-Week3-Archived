209507ed9be83144287c67fd5a9dd29003fb75bf
Know-Thyself <67852769+Know-Thyself@users.noreply.github.com>
2021-01-28 21:43:32 +0000

    Level 2 challenge is also completed now.
    Stop and autoplay buttons have been added to complete the second part of the challenge.

--- mandatory/3-slideshow/slideshow.js
+++ mandatory/3-slideshow/slideshow.js
@@ -1,16 +1,25 @@
 // Write your code here
 // Selecting Elements and defining variables
 let slides = document.getElementsByClassName('slide');
-let visible = document.getElementsByClassName('slide-visible');
-let hidden = document.getElementsByClassName('slide-hidden')
 let forward = document.getElementById('forwardBtn');
 let back = document.getElementById('backBtn');
 let slideCounter = 0;
 const totalSlides = slides.length;
 
+// Selecting elements and defining variables for level 2 stop and autoplay buttons
+let autoBack = document.querySelector('#auto-back');
+let stopBtn =  document.querySelector('#stop');
+let autoForward =  document.querySelector('#auto-forward');
+
 // Event listeners
 forward.addEventListener('click', nextSlide);
 back.addEventListener('click', previousSlide);
+
+// Additional event listeners
+autoBack.addEventListener('click', autoReverse);
+stopBtn.addEventListener('click', stopAutoSlide);
+autoForward.addEventListener('click', autoPlay);
+
 
 // A function to hide every image apart from the one displayed
 function slideUpdater () {
@@ -39,4 +48,22 @@ function previousSlide() {
         slideCounter--;
     }
     slideUpdater();
+}
+
+// A function to autoplay previous slides
+let autoBk;
+function autoReverse() {
+   autoBk = setInterval(previousSlide, 2000);
+}
+
+// A function to autoplay forward
+let auto;
+function autoPlay() {
+    auto = setInterval(nextSlide, 2000);
+}
+
+// A function to stop autoplay 
+function stopAutoSlide() {
+   clearInterval(autoBk);
+   clearInterval(auto);
 }
