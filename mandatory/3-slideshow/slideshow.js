4a39de8ac442e63495db290ac48e9821d1a73df3
moniqueking57 <65675759+moniqueking57@users.noreply.github.com>
2021-01-28 20:20:32 +0000

    css done on the quotes

--- mandatory/3-slideshow/slideshow.js
+++ mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,8 @@
 // Write your code here
+let nextBtn = document.getElementById('nextBtn');
+let backBtn = document.getElementById('backBtn');
+let pics = document.getElementById('image')
+
+nextBtn.addEventListener('click', e =>{
+
+})
