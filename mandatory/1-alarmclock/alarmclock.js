8f6bccfcbcf77b8fde44100045a6c2bf837b9f2a
SenaitEkubai <73396195+SenaitEkubai@users.noreply.github.com>
2021-01-29 00:39:22 +0000

    alarm clock exercise

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,5 +1,46 @@
-function setAlarm() {}
+let inputAlarmSet = document.getElementById("alarmSet");
+let timeRemaining = document.getElementById("timeRemaining");
+let setButton = document.getElementById("set");
+let time = inputAlarmSet.value;
+var intervalId;
 
+/* function setAlarm() {
+  time = inputAlarmSet.value;
+  timeRemaining.innerHTML = `time remaining ${time}`;
+  intervalId = setInterval(decreaseTimeByOne, 1000);
+}
+function decreaseTimeByOne() {
+  time--;
+  timeRemaining.innerHTML = `time remaining ${time}`;
+  if (time === 0) {
+    playAlarm();
+    clearInterval(intervalId);
+  }
+}
+ */
+
+var minutes;
+var seconds;
+var paddedMinutes;
+var paddedSeconds;
+function setAlarm() {
+  time = inputAlarmSet.value;
+  minutes = String(Math.floor(time / 60));
+  paddedMinutes = minutes.padStart(2, "0");
+  seconds = String(time % 60);
+  paddedSeconds = seconds.padStart(2, "0");
+  timeRemaining.innerHTML = `Time remaining: ${paddedMinutes}:${paddedSeconds}`;
+  intervalId = setInterval(decreaseTimeByOne, 1000);
+}
+function decreaseTimeByOne(time) {
+  //paddedMinutes--;
+  paddedSeconds = paddedSeconds -= 1;
+  timeRemaining.innerHTML = `Time remaining: ${paddedMinutes}:${paddedSeconds}`;
+  if (time === 0) {
+    playAlarm();
+    clearInterval(intervalId);
+  }
+}
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
