90871a1352a5637b54b2243987e654d9c2547cf1
SenaitEkubai <73396195+SenaitEkubai@users.noreply.github.com>
2021-01-29 01:28:04 +0000

    organised functions and eventlisteneres

--- mandatory/3-slideshow/slideshow.js
+++ mandatory/3-slideshow/slideshow.js
@@ -2,8 +2,8 @@
 var content = document.getElementById("container");
 
 var myImages = [
-  "../example-screenshots/image1.jpg",
   "../example-screenshots/image2.jpg",
+  "../example-screenshots/image1.jpg",
   "../example-screenshots/image3.jpg",
   "../example-screenshots/image4.jpg",
   "../example-screenshots/image5.jpg",
@@ -18,27 +18,35 @@ let autoBack = document.getElementById("auto-back");
 let autoNext = document.getElementById("auto-next");
 let stop = document.getElementById("stop");
 let image = document.querySelector("img").src;
+
 let i = myImages.length;
-// event listener for next button
-next.addEventListener("click", () => {
+
+// all functions
+
+// next function
+
+var nextFunction = function () {
   if (i < myImages.length) {
     i++;
   } else {
     i = 1;
   }
   content.innerHTML = "<img src =myImages/" + myImages[i - 1] + ">";
-});
-// prev button event listener
-prev.addEventListener("click", () => {
+};
+
+// prev function
+
+var prevFunction = function () {
   if (i < myImages.length + 1 && i > 1) {
     i--;
   } else {
     i = myImages.length;
   }
   content.innerHTML = "<img src =myImages/" + myImages[i - 1] + ">";
-});
-// when the auto back button is pressed the function inside the setInterval will be executed every 5 seconds
-autoBack.addEventListener("click", () => {
+};
+
+// autoback function
+var autoBackFunction = function () {
   setInterval(() => {
     if (i < myImages.length + 1 && i > 1) {
       i--;
@@ -46,11 +54,9 @@ autoBack.addEventListener("click", () => {
       i = myImages.length;
     }
     content.innerHTML = "<img src =myImages/" + myImages[i - 1] + ">";
-  }, 5000);
-});
-//auto next button event listener. when button is pressed the set interval inside is triggered and the code inside the set interval is executed every 5 seconds
-
-autoNext.addEventListener("click", () => {
+  }, 2000);
+};
+var autoNextFunction = function () {
   setInterval(() => {
     if (i < myImages.length) {
       i++;
@@ -59,8 +65,16 @@ autoNext.addEventListener("click", () => {
     }
     content.innerHTML = "<img src =myImages/" + myImages[i - 1] + ">";
   }, 2000);
-});
+};
+// all event listeners
+
+next.addEventListener("click", nextFunction);
+prev.addEventListener("click", prevFunction);
+autoBack.addEventListener("click", autoBackFunction);
+autoNext.addEventListener("click", autoNextFunction);
+
 // stop button event listener
+
 stop.addEventListener("click", () => {
-  content.innerHTML = "<img src =myImages/" + image + ">";
+  content.innerHTML = "<img src = example-screenshots/image2.jpg>";
 });
