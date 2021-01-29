0586e5435bbaceb5d7b3a7f7222ab62093e187f5
haroon iqbal <h.iqbal0121@gmail.com>
2021-01-29 20:23:13 +0000

    1 & 2

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,36 @@
-function setAlarm() {}
+function setAlarm() {
+  let remaining = document.getElementById("timeRemaining");
+  let setter = document.getElementById("alarmSet").value;
+
+  let minutes = Math.floor(setter / 60);
+  let seconds = setter % 60;
+
+  let timeInterval = setInterval(() => {
+    if (minutes > 0 && seconds === 0) {
+      minutes--;
+      seconds = 59;
+    }
+
+    if (seconds === 0) {
+      clearInterval(timeInterval);
+      audio.play();
+
+      let div = document.querySelector(".centre");
+      function changeColour() {
+        if (div.style.backgroundColor === "red") {
+          div.style.backgroundColor = "blue";
+        } else {
+          div.style.backgroundColor = "red";
+        }
+      }
+
+      setInterval(changeColour, 500);
+    }
+
+    remaining.textContent = `Time Remaining: ${minutes} : ${seconds}`;
+    seconds--;
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
