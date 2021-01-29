2d615f8a216cc83ed612b967afb0f9c05d3114ea
SenaitEkubai <73396195+SenaitEkubai@users.noreply.github.com>
2021-01-29 12:39:19 +0000

    added final adjustments

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,46 +1,38 @@
+/*This is an alarm clock which takes user input  */
 let inputAlarmSet = document.getElementById("alarmSet");
+inputAlarmSet.placeholder = "enter valid time value"; // added placeholder
 let timeRemaining = document.getElementById("timeRemaining");
 let setButton = document.getElementById("set");
 let time = inputAlarmSet.value;
-var intervalId;
 
-/* function setAlarm() {
-  time = inputAlarmSet.value;
-  timeRemaining.innerHTML = `time remaining ${time}`;
-  intervalId = setInterval(decreaseTimeByOne, 1000);
+function setAlarm() {
+  time = inputAlarmSet.value; // will update time when function is called
+  if (time > 0) {
+    // time is written in time format 00:00 using padStart() because this function only works with strings time is first converted to string
+    timeRemaining.innerHTML = `Time remaining: ${String(
+      Math.floor(time / 60)
+    ).padStart(2, "0")}:${String(Math.floor(time % 60)).padStart(2, "0")}`;
+    intervalId = setInterval(decreaseTimeByOne, 1000);
+  }
 }
+
+// time decrement function
+
 function decreaseTimeByOne() {
   time--;
-  timeRemaining.innerHTML = `time remaining ${time}`;
-  if (time === 0) {
-    playAlarm();
-    clearInterval(intervalId);
-  }
-}
- */
+  timeRemaining.innerHTML = `Time remaining: ${String(
+    Math.floor(time / 60)
+  ).padStart(2, "0")}:${String(Math.floor(time % 60)).padStart(2, "0")}`;
 
-var minutes;
-var seconds;
-var paddedMinutes;
-var paddedSeconds;
-function setAlarm() {
-  time = inputAlarmSet.value;
-  minutes = String(Math.floor(time / 60));
-  paddedMinutes = minutes.padStart(2, "0");
-  seconds = String(time % 60);
-  paddedSeconds = seconds.padStart(2, "0");
-  timeRemaining.innerHTML = `Time remaining: ${paddedMinutes}:${paddedSeconds}`;
-  intervalId = setInterval(decreaseTimeByOne, 1000);
-}
-function decreaseTimeByOne(time) {
-  //paddedMinutes--;
-  paddedSeconds = paddedSeconds -= 1;
-  timeRemaining.innerHTML = `Time remaining: ${paddedMinutes}:${paddedSeconds}`;
+  // if statement when time reaches zero
+
   if (time === 0) {
     playAlarm();
-    clearInterval(intervalId);
+    clearInterval(intervalId); // clear set interval using interval id;
+    inputAlarmSet.value = ""; // clear input field after clearing interval
   }
 }
+
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
