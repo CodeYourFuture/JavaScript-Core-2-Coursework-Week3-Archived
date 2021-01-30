050b9dba1a6ae5f532ddd78c95ebc83f8b8fcc4d
JK1320 <73619227+JK1320@users.noreply.github.com>
2021-01-30 00:10:40 +0000

    Complete-Mandatory - 2, 3

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,20 +1,46 @@
+let setAlarmBtn = document.getElementById("set");
+let h1 = document.getElementById("timeRemaining");
+let input = document.getElementById("alarmSet").value;
+
 function setAlarm() {
-  let setAlarmBtn = document.getElementById("set");
-  let h1 = document.getElementById("timeRemaining");
+  
   let stopBtn = document.getElementById("stop");
- 
+  let startingMinutes = 0;
+
    setAlarmBtn.addEventListener("click", function () {
-    let input = document.getElementById("alarmSet");
-    let minutes;
-    let seconds = 
-    h1.innerHTML = input.value
+    let min = input.value
+    h1 = min * 60;
+    console.log(min + "" + h1);
+    var t = setInterval(updateTime, 1000);
+    
+    function updateTime() {
+    let minutes2 = Math.floor(time / 60)
+    let seconds = time % 60;
+
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    };
+    if (min < 10) {
+      minutes2 = "0" + minutes2;
+    };
+    h1.innerHTML = `minutes2 : seconds`;
+    h1 --;
+    if(h1 === 0) {
+      clearInterval(t);
+    }
+    }
+     
+   
+   
     // function timeEverySecond () {
     // }
 
   //   setInterval(timeEverySecond, 1000);
-  //   h1.innerHTML = `Time Remaining: ${input.value * 10}`;
+  //   h1.innerHTML = `Time Remaining: ${minutes}:${seconds}`;
   //  })
-}
+})
+
+
 
 // DO NOT EDIT BELOW HERE
 
--- mandatory/3-slideshow/slideshow.js
+++ mandatory/3-slideshow/slideshow.js
@@ -6,26 +6,63 @@ let stopBtn = document.getElementById("stop");
 let autoBackBtn = document.getElementById("auto-back");
 let autoForwardBtn = document.getElementById("auto-forward");
 
-let imagesArr = ["img2.jpg", "img3.jpg", "img4.jpg"];
-console.log(imagesArr)
-var i = 0;
+let imagesArr = ["images/img1.jpg", "images/img2.jpg", "images/img3.jpg", "images/img4.jpg"];
 
+let i = 0;
+img.src = imagesArr[i];
+
+//Back Button
 backBtn.addEventListener("click", function () {
-    if (i <= 0) {
-    i = imagesArr.length;
+    if (i === 0) {
+    i = imagesArr.length - 1;
+} else {
     i --;
-    //return setImg();
 }
+img.src = imagesArr[i];
 });
 
+//forward Button
 forwardBtn.addEventListener("click", function () {
-    if (i >= imagesArr.length-1) {
-    i = -1;
+    if (i < imagesArr.length-1) {
     i ++;
-    //return setImg;
+} else {
+    i = 0;
 }
+img.src = imagesArr[i];
 });
 
-// function setImg() {
-//     return img.setAttribute("src", "img/" + imagesArr[i]);
-// };
+// Auto Forward Button
+let stopAutoForward;
+autoForwardBtn.addEventListener("click", function () {
+stopAutoForward = setInterval(function () {
+ if (i < imagesArr.length-1) {
+    i ++;
+} else {
+    i = 0;
+}
+img.src = imagesArr[i];
+}, 2000);
+});
+
+//Auto back Button
+let stopAutoBack;
+autoBackBtn.addEventListener("click", function () {
+stopAutoBack = setInterval(function () {
+  if (i === 0) {
+    i = imagesArr.length - 1;
+} else {
+    i --;
+}
+img.src = imagesArr[i];
+}, 2000);
+})
+
+//Stop Button
+stopBtn.addEventListener("click", function () {
+    clearInterval(stopAutoBack);
+    clearInterval(stopAutoForward);
+})
+
+
+
+
