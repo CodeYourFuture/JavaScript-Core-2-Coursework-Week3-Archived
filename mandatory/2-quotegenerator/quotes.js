a3715d2f134791ae1ef1847e9ab96256ab3bb450
Adepeju18 <73551379+Adepeju18@users.noreply.github.com>
2021-02-02 21:17:06 +0000

    js2 week 3exercise

--- mandatory/2-quotegenerator/quotes.js
+++ mandatory/2-quotegenerator/quotes.js
@@ -1,33 +1,6 @@
  let btn = document.getElementById("btn");
  let output = document.getElementById("output");
 let author = document.getElementById("author");
-// let quotes =[
-//    `"Life isn’t about getting and having, it’s about giving and being."-
-//      "Kevin Kruse"`,
-  
-  
-//   `"Whatever the mind of man can conceive and believe, it can achieve."-
-//     "Napoleon Hill"`,
-  
-//   `"Strive not to be a success, but rather to be of value."-
-//    "Albert Einstein"`,
-//       `"Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference."-
-//        "Robert Frost"`,
-//    `"I attribute my success to this: I never gave or took any excuse."-
-//    "Florence Nightingale"`,
-//     `"You miss 100% of the shots you don’t take."- "Wayne Gretzky"`,
-//       `"I’ve missed more than 9000 shots in my career. I’ve lost almost 300 games. 26 times I’ve been trusted to take the game winning shot and missed. I’ve failed over and over and over again in my life. And that is why I succeed."-
-//       "Michael Jordan"`,
-//       `"The most difficult thing is the decision to act, the rest is merely tenacity."-
-//       "Amelia Earhart"`,
-//    `"Every strike brings me closer to the next home run."-"Babe Ruth"`,
-//     `"Definiteness of purpose is the starting point of all achievement."-
-// "W. Clement Stone"`,
-//  `"We must balance conspicuous consumption with conscious capitalism."-
-//     "Kevin Kruse"`,
-//   `"Life is what happens to you while you’re busy making other plans."-
-//     "John Lennon"`,
-//];
 btn.addEventListener("click",function(){
   var randomQuote = quotes[Math.floor(Math.random()*quotes.length)];
   output.innerHTML = randomQuote.quote;
