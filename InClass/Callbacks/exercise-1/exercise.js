d9949a4e2883be62204ef645ab1d6d73be089484
Know-Thyself <67852769+Know-Thyself@users.noreply.github.com>
2021-02-03 06:41:40 +0000

    Classwork updates

--- InClass/Callbacks/exercise-1/exercise.js
+++ InClass/Callbacks/exercise-1/exercise.js
@@ -11,3 +11,19 @@ Update your code to make the colour change every 5 seconds to something differen
 Prefer to work on a codepen? https://codepen.io/makanti/pen/abOreLg
 ================
 */
+
+mainPage = document.querySelector('#main');
+// Task 1 - A function to change the background color after 5 seconds
+setTimeout(function() {
+    document.body.style.backgroundColor = 'blue';
+}, 5000);
+
+// Task 2 - An updated function to change the background color every 5 seconds
+var i = 0;
+function changeBgColor() {
+  var color = ["blue", "white", "green", "yellow", "gray"];
+  document.body.style.backgroundColor = color[i];
+  i = (i + 1) % color.length;
+}
+setInterval(changeBgColor, 5000);
+
