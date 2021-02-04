d92b9215815119aae0f508170af5e17722131a4a
samia-pk <samiaarshad92@gmail.com>
2021-02-04 22:28:08 +0000

    slideshow exercise continue...

--- mandatory/3-slideshow/slideshow.js
+++ mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,21 @@
 // Write your code here
+let nextBtn = document.getElementById('nextBtn');
+let backBtn = document.getElementById('backBtn');
+
+let pics = document.getElementById('images')
+
+let counter = 0;
+
+nextBtn.addEventListener('click', e =>{
+   if(counter<5){
+    counter++;
+    document.getElementById('images').src = `/mandatory/3-slideshow/Images/img${counter}.jpeg`; 
+  }
+})
+
+backBtn.addEventListener('click', e =>{
+   if(counter>1){
+    counter--;
+    document.getElementById('images').src = `/mandatory/3-slideshow/Images/img${counter}.jpeg`; 
+  }
+})
