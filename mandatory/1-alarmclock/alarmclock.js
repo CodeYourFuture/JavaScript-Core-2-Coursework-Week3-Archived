3dbc123906ecd618e00a590afdc0c05ec3d42420
remidoukmak <73621087+remidoukmak@users.noreply.github.com>
2021-02-11 12:52:15 +0000

    Add files via upload

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,60 @@
-function setAlarm() {}
+let countdown;
+let header;
+let myVar;
+
+function setAlarm() {
+  header = document.getElementById("timeRemaining");
+  //console.log(header.innerText);
+  let time = document.getElementById("alarmSet");
+  //console.log(time.value);
+  countdown = time.value * 1000;
+  myVar = setInterval(myTimer, 1000);
+}
+
+function convertSeconds(s){
+  var min = floor(s/60);
+  var sec = s % 60;
+  return nf(min,2) + ":" + nf(sec, 2);
+
+  var params = getURLParams();
+  console.log(params);
+  if (params.minute){
+    var min = params.minute;
+    timeleft = min * 60;
+  }
+
+}
+
+function myTimer() {
+  countdown = countdown - 1000;
+  //console.log("timesLeft" + countdown);
+  let secondsRemaining = countdown / 1000;
+  let timeText = "";
+  if (secondsRemaining < 10) {
+    timeText = "0" + secondsRemaining;
+  }
+  else{
+    timeText = secondsRemaining;
+  }
+  header.innerText = "Time Remaining " + timeText;
+  if (countdown === 0) {
+   // playAlarm();
+   console.log("ALARM");
+    clearInterval(myVar);
+  }
+}
+
+// var myVar = setInterval(myTimer, 1000);
+
+// function myTimer() {
+//   var d = new Date();
+//   var t = d.toLocaleTimeString();
+//   document.getElementById("demo").innerHTML = t;
+// }
+
+// function myStopFunction() {
+//   clearInterval(myVar);
+// }
 
 // DO NOT EDIT BELOW HERE
 
@@ -23,3 +79,8 @@ function pauseAlarm() {
 }
 
 window.onload = setup;
+
+function addZero(time) {
+  return time < 10 ? "0" + time : time;
+}
+
