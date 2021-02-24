7ae30d9fecd034a5b4824f740c979ca9c79548ba
Bahia369 <hsbcoding@gmail.com>
2021-02-24 15:28:41 +0000

    All 3 Mandatory completed
    Mandatory is completed and code is running.

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,25 +1,58 @@
-function setAlarm() {}
+function setAlarm() {
+    let remaining = document.getElementById("timeRemaining");
+    let setter = document.getElementById("alarmSet").value;
+
+    let minutes = Math.floor(setter / 60);
+    let seconds = setter % 60;
+
+    let timeInterval = setInterval(() => {
+        if (minutes > 0 && seconds === 0) {
+            minutes--;
+            seconds = 59;
+        }
+
+        if (seconds === 0) {
+            clearInterval(timeInterval);
+            audio.play();
+
+            let div = document.querySelector(".centre");
+
+            function changeColour() {
+                if (div.style.backgroundColor === "green") {
+                    div.style.backgroundColor = "yellow";
+                } else {
+                    div.style.backgroundColor = "blue";
+                }
+            }
+
+            setInterval(changeColour, 500);
+        }
+
+        remaining.textContent = `Time Remaining: ${minutes} : ${seconds}`;
+        seconds--;
+    }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
 
 function setup() {
-  document.getElementById("set").addEventListener("click", () => {
-    setAlarm();
-  });
+    document.getElementById("set").addEventListener("click", () => {
+        setAlarm();
+    });
 
-  document.getElementById("stop").addEventListener("click", () => {
-    pauseAlarm();
-  });
+    document.getElementById("stop").addEventListener("click", () => {
+        pauseAlarm();
+    });
 }
 
 function playAlarm() {
-  audio.play();
+    audio.play();
 }
 
 function pauseAlarm() {
-  audio.pause();
+    audio.pause();
 }
 
 window.onload = setup;
--- mandatory/3-slideshow/slideshow.js
+++ mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,62 @@
 // Write your code here
+// array of pictures
+const arrayOfPictures = [
+    "https://images.pexels.com/photos/2555589/pexels-photo-2555589.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    "https://images.pexels.com/photos/6931284/pexels-photo-6931284.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    "https://images.pexels.com/photos/3483966/pexels-photo-3483966.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    "https://images.pexels.com/photos/6341413/pexels-photo-6341413.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+];
+
+// html button selection linked in with html
+const nextBtn = document.getElementById("next");
+const backBtn = document.getElementById("back");
+const image = document.getElementById("image");
+const autoBack = document.getElementById("backAuto");
+const autoNext = document.getElementById("nextAuto");
+const stopBtn = document.getElementById("stop");
+
+// state/Global variabes
+let currentSlide = 0;
+let newNext;
+let newBack;
+// slide forward function
+function slidesForward() {
+    if (currentSlide === arrayOfPictures.length - 1) {
+        currentSlide = 0;
+        image.src = arrayOfPictures[0];
+    } else {
+        currentSlide++;
+        image.src = arrayOfPictures[currentSlide];
+    }
+}
+
+// Slide Backward function/ if and else
+function slidesBackward() {
+    if (currentSlide <= 0) {
+        image.src = arrayOfPictures[arrayOfPictures.length - 1];
+        currentSlide = arrayOfPictures.length - 1;
+    } else {
+        currentSlide--;
+        image.src = arrayOfPictures[currentSlide];
+    }
+}
+// eventlistener for clicks auto next
+autoNext.addEventListener("click", () => {
+    clearInterval(newNext);
+    clearInterval(newBack);
+    newNext = setInterval(slidesForward, 4000);
+});
+// events listenr to go back
+autoBack.addEventListener("click", () => {
+    clearInterval(newBack);
+    clearInterval(newNext);
+    newBack = setInterval(slidesBackward, 4000);
+});
+// stop button works on the interval
+stopBtn.addEventListener("click", () => {
+    clearInterval(newBack);
+    clearInterval(newNext);
+});
+// normal button back and next
+nextBtn.addEventListener("click", slidesForward);
+backBtn.addEventListener("click", slidesBackward);
