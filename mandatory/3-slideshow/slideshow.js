4265a01ac660f932c1e10a7c6eb233c7a7352cc3
Fatimohkuforiji1 <fatimohkuforiji@yahoo.com>
2021-02-25 05:13:19 +0000

    Added answers

--- mandatory/3-slideshow/slideshow.js
+++ mandatory/3-slideshow/slideshow.js
@@ -1,93 +1,102 @@
 // Write your code here
 
-const images = [
-    "https://cdn11.bigcommerce.com/s-tf2vn6/product_images/uploaded_images/pink-and-roses.jpg",
-    "https://miro.medium.com/max/2560/1*aSVcqYXfcjpmIgBkBI4m3w.png",
-    "https://dy1yydbfzm05w.cloudfront.net/media/folk/homepageblocks/image//h/p/hp-grid-01_2x_21.jpg",
-    "https://www.growerdirect.com/uploads/thumbnails/flower%20varieties.jpg.7de9d680.jpg"
-]
+let images = [
+  {
+    imageName: "Books",
+    imageLink:
+      "https://images.unsplash.com/photo-1503676260728-1c00da094a0b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8c2Nob29sfGVufDB8fDB8&ixlib=rb-1.2.1&w=1000&q=80"
+  },
+  {
+    imageName: "Furniture classroom",
+    imageLink:
+      "https://images.unsplash.com/photo-1580582932707-520aed937b7b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTR8fHNjaG9vbHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"
+  },
+  {
+    imageName: "Lecture Hall",
+    imageLink:
+      "https://images.unsplash.com/photo-1606761568499-6d2451b23c66?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzN8fHNjaG9vbHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"
+  },
+  {
+    imageName: "Pencils",
+    imageLink:
+      "https://images.unsplash.com/photo-1516383607781-913a19294fd1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NTR8fHNjaG9vbHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"
+  },
+];
+console.log(images.length);
 
-const carouselDiv = document.querySelector(".carousel");
-const ctrlDiv = document.querySelector(".carousel-buttons");
-const docFrag = document.createDocumentFragment();
-const backButton = document.createElement("button");
-const forwardButton = document.createElement("button");
-const autoBackButton = document.createElement("button");
-const autoForwardButton = document.createElement("button");
-const stopButton = document.createElement("button");
-const headerText = document.createElement("h1");
+let myDivImg = document.querySelector(".imgDiv");
+let myH1 = document.createElement("h1");
+myH1.innerText = "My Image Carousel";
+myDivImg.appendChild(myH1);
+let defaultImg = document.createElement("img");
+defaultImg.src = images[0]["imageLink"];
+myDivImg.appendChild(defaultImg);
+let imgNam = document.createElement("h4");
+imgNam.innerText = images[0]["imageName"];
+myDivImg.appendChild(imgNam);
 
+let divBtn = document.querySelector(".btnDiv");
+myDivImg.appendChild(divBtn);
 
-headerText.textContent = "Image Carousel";
-backButton.textContent = "Back";
-forwardButton.textContent = "Forward";
-backButton.className = "controlButton";
-forwardButton.className = "controlButton";
-autoBackButton.textContent = "Auto Back";
-autoBackButton.className = "controlButton";
-autoForwardButton.textContent = "Auto Forward";
-autoForwardButton.className = "controlButton";
-stopButton.textContent = "Stop";
-stopButton.className = "controlButton";
+let backBtn = document.createElement("button");
+backBtn.innerText = "Back";
+divBtn.appendChild(backBtn);
 
-// ctrlDiv.appendChild(autoBackButton);
-// ctrlDiv.appendChild(backButton);
-// ctrlDiv.appendChild(stopButton)
-// ctrlDiv.appendChild(forwardButton);
-// ctrlDiv.appendChild(autoForwardButton);
+let autoBackBtn = document.createElement("button");
+autoBackBtn.innerText = "Auto Back";
+divBtn.appendChild(autoBackBtn);
 
-document.body.prepend(headerText);
-document.body.insertBefore(headerText, carouselDiv);
-docFrag.appendChild(autoBackButton);
-docFrag.appendChild(backButton);
-docFrag.appendChild(stopButton)
-docFrag.appendChild(forwardButton);
-docFrag.appendChild(autoForwardButton);
+let stopBtn = document.createElement("button");
+stopBtn.innerText = "Stop";
+divBtn.appendChild(stopBtn);
 
-ctrlDiv.appendChild(docFrag)
+let forwardBtn = document.createElement("button");
+forwardBtn.innerText = "Forward";
+divBtn.appendChild(forwardBtn);
 
-const defaultImg = document.createElement("img");
-defaultImg.src = images[0];
-defaultImg.alt = "flower";
-carouselDiv.appendChild(defaultImg)
+let autoForwardBtn = document.createElement("button");
+autoForwardBtn.innerText = "Auto Forward";
+divBtn.appendChild(autoForwardBtn);
 
 let counter = 0;
-let stopInterval;
+let stopBtnInterval;
 
-function forwardCarousel() {
-    counter += 1;
-    counter = counter % images.length;
-    defaultImg.src = images[counter];
+function forwardBtnFunc() {
+  counter += 1;
+  counter = counter % images.length;
+  defaultImg.src = images[counter]["imageLink"];
+  imgNam.innerText = images[counter]["imageName"];
+  console.log(counter);
 }
 
-function backCarousel() {
-    if (counter === 0) {
-        counter = images.length;
-    }
+function backBtnFunc() {
+  if (counter === 0) {
+    counter = images.length - 1;
+  } else {
     counter -= 1;
-    defaultImg.src = images[counter];
+  }
+  // counter = counter % images.length;
+  defaultImg.src = images[counter]["imageLink"];
+  imgNam.innerText = images[counter]["imageName"];
+  console.log(counter);
 }
 
-function autoBackCarousel() {
-    clearInterval(stopInterval);
-    stopInterval = setInterval(backCarousel, 2000);
+function stopBtnFunc() {
+  clearInterval(stopBtnInterval);
 }
 
-function stopCarousel() {
-    clearInterval(stopInterval);
+function autoForwardFunc() {
+  stopBtnFunc();
+  stopBtnInterval = setInterval(forwardBtnFunc, 1000);
 }
 
-function autoForwardCarousel() {
-    clearInterval(stopInterval);
-    stopInterval = setInterval(forwardCarousel, 2000);
+function autoBackFunc() {
+  stopBtnFunc();
+  stopBtnInterval = setInterval(backBtnFunc, 1000);
 }
 
-forwardButton.addEventListener("click", forwardCarousel);
-
-backButton.addEventListener("click", backCarousel);
-
-autoBackButton.addEventListener("click", autoBackCarousel);
-
-autoForwardButton.addEventListener("click", autoForwardCarousel);
-
-stopButton.addEventListener("click", stopCarousel);
+forwardBtn.addEventListener("click", forwardBtnFunc);
+backBtn.addEventListener("click", backBtnFunc);
+stopBtn.addEventListener("click", stopBtnFunc);
+autoForwardBtn.addEventListener("click", autoForwardFunc);
+autoBackBtn.addEventListener("click", autoBackFunc);
