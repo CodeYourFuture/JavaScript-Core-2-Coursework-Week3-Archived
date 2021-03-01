8244db51fd92feea5414c4c62e3aa2ecdcbb0af1
askinekinci <askinekincihf@gmail.com>
2021-03-01 21:52:39 +0000

    completed Callbacks/exercise-1

--- InClass/Callbacks/exercise-1/exercise.js
+++ InClass/Callbacks/exercise-1/exercise.js
@@ -11,3 +11,31 @@ Update your code to make the colour change every 5 seconds to something differen
 Prefer to work on a codepen? https://codepen.io/makanti/pen/abOreLg
 ================
 */
+
+// Task -1
+const body = document.querySelector("body");
+function changeBodyColor() {
+  body.style.backgroundColor = "green"
+}
+setTimeout(changeBodyColor, 5000);
+
+
+// Task -2
+colors = ["green", "yellow", "blue", "purple", "pink", "coral", "lightblue", "orange"];
+function randomNumber(){
+  const number = Math.floor(Math.random() * colors.length);
+  return number;
+};
+
+function changeBodyColor1() {
+  body.style.backgroundColor = colors[randomNumber()];
+  randomNumber();
+  console.log(randomNumber());
+};
+
+let interval = setInterval(changeBodyColor1, 1000);
+
+setTimeout(() => {
+  clearInterval(interval);
+  body.style.backgroundColor = "white";
+}, 10000);
