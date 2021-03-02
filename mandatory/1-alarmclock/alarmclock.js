7a39897710038281a92523436a3ff5619dcccc6e
Dan <danielghanbari7@gmail.com>
2021-03-02 10:55:18 +0000

    1-alarmclock

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,42 @@
-function setAlarm() {}
+function setAlarm() {
+  const inputEl = document.getElementById("alarmSet").value;
+  let myTimer = inputEl;
+
+  let timeInterval = setInterval(() => {
+    const timeDisplay = document.getElementById("timeRemaining");
+    let min = Math.floor(myTimer / 60);
+    let sec = myTimer - min * 60;
+
+    if (sec < 10) {
+      sec = "0" + sec;
+    }
+    if (min < 10) {
+      min = "0" + min;
+    }
+    if (myTimer === 0) {
+      playAlarm();
+      clearInterval(timeInterval);
+      backgroundColor();
+    }
+    let message = min + ":" + sec;
+    timeDisplay.innerText = `Time Remaining: ${message}`;
+    myTimer--;
+  }, 1000);
+}
+// Background color flash
+function backgroundColor() {
+  const xxx = setInterval(() => {
+    let color1 = Math.floor(Math.random() * 256);
+    let color2 = Math.floor(Math.random() * 256);
+    let color3 = Math.floor(Math.random() * 256);
+    let RGB = `rgb(${color1}, ${color2}, ${color3})`;
+    document.getElementsByTagName("body")[0].style.backgroundColor = RGB;
+  }, 1000);
+}
+
+function pauseFlash() {
+  location.reload();
+}
 
 // DO NOT EDIT BELOW HERE
 
@@ -11,6 +49,9 @@ function setup() {
 
   document.getElementById("stop").addEventListener("click", () => {
     pauseAlarm();
+  });
+  document.getElementById("stopBgColor").addEventListener("click", () => {
+    pauseFlash();
   });
 }
 
