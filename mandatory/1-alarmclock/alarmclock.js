0c8c9c08c962d9de3b5e2c2b72bae484eae6e872
alinosratipour <alinosratipour@gmail.com>
2021-03-04 20:27:14 +0000

    Finish all mandatory tasks I will look into it for improvment

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,7 +1,38 @@
-function setAlarm() {}
+
+function setAlarm() {
+  // selecting our html Elements
+  let inputSet = document.querySelector("#alarmSet").value;
+  let timer = document.querySelector("#digit");
+  
+
+  let minutes = Math.floor(inputSet / 60);
+  let seconds = inputSet % 60;
+
+ startCounting = setInterval(countDown,1000);
+
+function countDown(){
+  
+  if (minutes > 0 && seconds === 0) {
+      minutes--;
+      seconds = 60;
+    
+    }
+
+  if (seconds === 0) {
+    clearInterval(startCounting);
+    audio.play();
+
+  }
+   timer.innerText = ` ${minutes} : ${seconds}`;
+     seconds--; 
+}
+
+
+}
+
 
 // DO NOT EDIT BELOW HERE
-// start coding tommarow
+
 var audio = new Audio("alarmsound.mp3");
 
 function setup() {
--- mandatory/3-slideshow/slideshow.js
+++ mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,83 @@
-// Write your code here
+let imageEl = document.querySelector("#myImg");
+let fname = document.querySelector(".flowerName");
+
+const flowers = [
+  {
+    name: " 0 flower",
+    src: "img/1.jpg",
+  },
+
+  {
+    name: "1 flower",
+    src: "img/2.jpg",
+  },
+
+  {
+    name: " 2 flower",
+    src: "img/3.jpg",
+  },
+
+  {
+    name: "3 flower",
+    src: "img/4.jpg",
+  },
+
+  {
+    name: "4 flower",
+    src: "img/5.jpg",
+  },
+];
+
+imageEl.src = flowers[0].src;
+fname.innerText = flowers[0].name;
+let i = 0;
+
+function forward() {
+  i++;
+
+  if (i < flowers.length) {
+    imageEl.src = flowers[i].src;
+    imageEl.classList.add("br") ;
+    fname.innerText = flowers[i].name;
+  } else {
+    i = 0;
+    imageEl.src = flowers[i].src;
+    fname.innerText = flowers[i].name;
+  }
+}
+
+function backWard() {
+  i--;
+
+  if (i >= 0) {
+    imageEl.src = flowers[i].src;
+    fname.innerText = flowers[i].name;
+  } else {
+    i = flowers.length - 1;
+    imageEl.src = flowers[i].src;
+    fname.innerText = flowers[i].name;
+  }
+}
+
+let start, startBack;
+
+document.querySelector(".forward").addEventListener("click", forward);
+document.querySelector(".backward").addEventListener("click", backWard);
+
+document.querySelector(".autoForward").addEventListener("click", () => {
+  if (startBack) {
+    clearInterval(startBack);
+  }
+  start = setInterval(forward, 2000);
+});
+
+document.querySelector(".autoBackward").addEventListener("click", () => {
+  if (start) {
+    clearInterval(start);
+  }
+  startBack = setInterval(backWard, 2000);
+});
+
+document.querySelector(".stop").addEventListener("click", () => {
+  clearInterval(start);
+});
