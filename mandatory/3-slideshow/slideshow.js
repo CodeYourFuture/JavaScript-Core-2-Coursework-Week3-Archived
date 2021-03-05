8065ea22deef6dc8b42a9e9ce18bf67ae7baa1ba
Ionut Tripa <ionut.tripa.23@gmail.com>
2021-03-05 16:12:47 +0000

    slideShow

--- mandatory/3-slideshow/slideshow.js
+++ mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,66 @@
 // Write your code here
+let arrayOfPictures = [
+    'https://images.pexels.com/photos/420233/pexels-photo-420233.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+    'https://images.pexels.com/photos/2023384/pexels-photo-2023384.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+    'https://images.unsplash.com/photo-1535930891776-0c2dfb7fda1a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1867&q=80',
+    'https://images.unsplash.com/photo-1598133894008-61f7fdb8cc3a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
+];
+
+let imgEl = document.querySelector('#imgSlide');
+let nextEl = document.querySelector('#next');
+let prevEl = document.querySelector('#prev');
+let autoForwEl = document.querySelector('#autoForw');
+let autoBackEL = document.querySelector('#autoBack');
+let stopEl = document.querySelector('#stop');
+
+let forward;
+let backward;
+
+
+imgEl.src = arrayOfPictures[0];
+let num = 0;
+
+let autoF;
+let autoB;
+
+function nextPic() {
+    if (num === arrayOfPictures.length - 1) {
+        num = -1;
+    }
+    imgEl.src = arrayOfPictures[num + 1];
+    num++;
+};
+
+function prevPic() {
+    if (num === 0) {
+        num = arrayOfPictures.length;
+    }
+    imgEl.src = arrayOfPictures[num - 1];
+    num--;
+}
+
+nextEl.addEventListener('click', nextPic);
+prevEl.addEventListener('click', prevPic);
+
+
+stopEl.addEventListener('click', function () {
+    clearInterval(autoF);
+    clearInterval(autoB);
+});
+
+autoForwEl.addEventListener('click', function () {
+    autoF = setInterval(nextPic, 2000);
+});
+
+autoBackEL.addEventListener('click', function () {
+    autoB = setInterval(prevPic, 2000);
+})
+
+
+
+
+// function imageSlide() {
+
+// }
+
+// imageSlide();
