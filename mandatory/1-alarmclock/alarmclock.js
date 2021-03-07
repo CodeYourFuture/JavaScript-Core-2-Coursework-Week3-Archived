d6a5ffa5099d86e71697356adb53fb155184cea9
yunusfirat <yunusfirat44@gmail.com>
2021-03-07 01:06:04 +0000

    1-alarmClock debugging

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,17 +1,23 @@
 function setAlarm() {
-  const set = document.querySelector('#set');
-  const stop = document.querySelector('#stop');
-  const input = document.querySelector('#alarmSet');
-  const timeRemanining = document.querySelector('#timeRemaining');
-  let inputValue = input.value;
-  let minutes = Math.floor(inputValue / 60);
-  let seconds = inputValue % 60;
-  seconds -=1;
-  set.addEventListener('click', () => {
-    timeRemanining.innerHTML = `Time Remaining: ${minutes}:${seconds}`;
+  let input = document.querySelector("#alarmSet").value;
+  let timeRemanining = document.querySelector("#timeRemaining");
+  let minutes = Math.floor(input / 60);
+  let seconds = input % 60;
+  timeRemanining.innerHTML = `Time Remaining: ${minutes.toLocaleString('en-GB', {minimumIntegerDigits: 2})}:${seconds.toLocaleString('en-GB', {minimumIntegerDigits: 2})}`;
+  document.querySelector("#stop").addEventListener("click", function(){
+    location.reload();
   })
+  setInterval(function () {
+    if (input === 0) {
+      playAlarm();
+    } else {
+      input--;
+      let minutes = Math.floor(input / 60);
+      let seconds = input % 60;
+      timeRemanining.innerHTML = `Time Remaining: ${minutes.toLocaleString('en-GB', {minimumIntegerDigits: 2})}:${seconds.toLocaleString('en-GB', {minimumIntegerDigits: 2})}`;
+    }
+  }, 1000);
 }
-
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
