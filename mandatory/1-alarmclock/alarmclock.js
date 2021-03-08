5b34aac514178d83678739bffa075f3e9835a1b4
Oksana Kostenko <kseniavlad31@gmail.com>
2021-03-08 18:01:06 +0000

    completes alarmclock

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,28 @@
-function setAlarm() {}
+const header = document.getElementById("timeRemaining");
+let remainingSeconds;
+let interval;
+
+function setAlarm() {
+  remainingSeconds = document.querySelector("#alarmSet").value;
+  render();
+  interval = setInterval(tick, 1000);
+}
+
+const render = () => {
+  let minutes = Math.floor(remainingSeconds / 60);
+  let seconds = remainingSeconds % 60;
+  header.innerText = `Time remaining ${minutes}:${seconds}`;
+};
+
+const tick = () => {
+  render();
+  remainingSeconds--;
+  if (remainingSeconds < 0) {
+    playAlarm();
+    clearInterval(interval);
+    header.innerText = `Time remaining: 00:00`;
+  }
+};
 
 // DO NOT EDIT BELOW HERE
 
