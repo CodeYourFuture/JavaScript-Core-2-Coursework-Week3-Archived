00e02caeb7a6196e7265eeaafd180933eb61584f
Simba Pfaira <spfaira@yahoo.com>
2021-03-10 21:09:20 +0200

    Completed inclass exercise 2

--- InClass/Callbacks/exercise-2/exercise.js
+++ InClass/Callbacks/exercise-2/exercise.js
@@ -2,38 +2,8 @@
 ================
 Exercise 2
 ----------
-You are given the following list of movies
+You are given the following list of movies */
 
-Task 1
-Create a function called "showMovies" that
-- iterates through the "movies" array and
-- for each movie, it creates a <p> element with the movie title and director and append it to the #all-movies div.
-- it sets the innerText of the #movies-number element to the total number of the movies in the array "movies"
-
-Task 2
-Amend your function above to only show movies after 1 second. Remember to use setTimeout to achieve that
-Create a new function called "addMovie"
-- it receives a movie object as an argument - your can create a new object for your favorite movie following using the "myMovies" objects as a guide 
-- it adds the new movie to the list of movies after 2 seconds. Remember to setTimeout to achieve that
-Call addMovies to add the new movie to the list and then showMovies to see the movies added on the screen.
-How many movies can you see on your page?
-
-Task 3
-Can you make sure the new movie you just added is showing on the screen? 
-TIP: use callbacks
-
-Task 4 - **Extra**
-Create a form anywhere on your page. The form should have
-- 4 input text fields, one for each property of your movie object
-- a "save" button.
-When the button is clicked
-- The field values should be used to create a new movie object literal
-- The new movie is then added to the list of movies and gets displayed on your page
-TIP: Use the functions you created on tasks 1-3
-
-Prefer to work on a codepen? https://codepen.io/makanti/pen/MWwMgmW?editors
-================
-*/
 const movies = [
   {
     title: "Color Out of Space",
@@ -61,10 +31,92 @@ const movies = [
   },
 ];
 
-// create showMovies function
+/*
+Task 1
+Create a function called "showMovies" that
+- iterates through the "movies" array and
+- for each movie, it creates a <p> element with the movie title and director and append it to the #all-movies div.
+- it sets the innerText of the #movies-number element to the total number of the movies in the array "movies" */
 
+function showMovies() {
+  let listEl = document.querySelector("#all-movies");
+  listEl.innerHTML = "";
 
-// create a new movie object for your favorite movie
+  movies.forEach((movie) => {
+    let listItem = document.createElement("li");
+    listItem.innerHTML = `${movie.title} - ${movie.director}`;
+    listEl.appendChild(listItem);
+  });
 
+  let quantitySpan = document.querySelector("#movies-number");
+  quantitySpan.innerHTML = `${movies.length}`;
+}
 
-// create addMovies function
+showMovies();
+
+/*
+Task 2
+Create a new function called "addMovie"
+- it receives a movie object as an argument - your can create a new object for your favorite movie following using the "myMovies" objects as a guide 
+- it adds the new movie to the list of movies after 2 seconds. Remember to setTimeout to achieve that
+Call addMovies to add the new movie to the list and then showMovies to see the movies added on the screen.
+How many movies can you see on your page? */
+
+function addMovie(movie, callback) {
+  setTimeout(function () {
+    movies.push(movie);
+    showMovies();
+  }, 2000);
+}
+const myFavMovie = {
+  title: "3 Idiots",
+  director: "Rancho Wangdu",
+  type: "comedy",
+  haveWatched: true, //thousand times
+};
+//addMovie(myFavMovie);
+
+/*
+Task 3
+Can you make sure the new movie you just added is showing on the screen? 
+TIP: use callbacks
+
+Task 4 - **Extra**
+- On the form with id #add-movies, add a "submit" event listener
+- When the button is clicked you need to:
+    - Read all input field values and create a new movie object literal with title, director, type and haveWatched properties
+    - Push the new object on the movies array 
+    - Display the updated list of movies on your page
+
+TIP: Use the functions you created on tasks 1-3
+================
+*/
+let submitButton = document.querySelector("#movie-submit");
+
+submitButton.addEventListener("click", function processData(event) {
+  event.preventDefault();
+  let titleEl = document.querySelector("#title");
+  let directorEl = document.querySelector("#director");
+  let typeEl = document.querySelector("#type");
+  let haveWatchedEl = document.querySelector("#haveWatched");
+
+  let movieTitle = titleEl.value;
+  let movieDirector = directorEl.value;
+  let movieType = typeEl.value;
+  let watchedStatus = haveWatchedEl.value;
+
+  if (movieDirector && movieDirector && movieType) {
+    let newMovie = {
+      title: movieTitle,
+      director: movieDirector,
+      type: movieType,
+      haveWatched: watchedStatus,
+    };
+
+    addMovie(newMovie);
+    titleEl.value = "";
+    directorEl.value = "";
+    typeEl.value = "";
+    haveWatchedEl.checked = false;
+  }
+});
