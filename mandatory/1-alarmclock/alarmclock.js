2ae4ce9b55d18c47864b2eb47854447e7b189278
Simba Pfaira <spfaira@yahoo.com>
2021-03-10 22:50:59 +0200

    Completed alarm clock exercise

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,41 @@
-function setAlarm() {}
+let alarmInterval, flashInterval;
+let body = document.querySelector("body");
+function setAlarm() {
+  let inputField = document.querySelector("#alarmSet");
+  let alarmTime = parseInt(inputField.value);
+  let timeLeftText = document.querySelector("#timeRemaining");
+  if (alarmTime) {
+    alarmInterval = setInterval(function () {
+      timeLeftText.innerHTML = `Time remaining: ${formatTime(alarmTime)}`;
+      alarmTime--;
+      if (alarmTime == -1) {
+        clearInterval(alarmInterval);
+        playAlarm();
+        let body = document.querySelector("body");
+        flashInterval = setInterval(function () {
+          var backgroundColour = body.style.backgroundColor;
+          body.style.backgroundColor =
+            backgroundColour === "red" ? "white" : "red";
+        }, 200);
+      }
+    }, 1000);
+
+    inputField.value = "";
+  }
+}
+
+function formatTime(time) {
+  let finalTime;
+  if (time > 59) {
+    let hours = Math.round(time / 60);
+    let minutes = time % 60;
+    return `${hours}:${minutes}`;
+  } else if (time < 10) {
+    return `00:0${time}`;
+  } else {
+    return `00:${time}`;
+  }
+}
 
 // DO NOT EDIT BELOW HERE
 
@@ -20,6 +57,8 @@ function playAlarm() {
 
 function pauseAlarm() {
   audio.pause();
+  clearInterval(alarmInterval);
+  clearInterval(flashInterval);
 }
 
 window.onload = setup;
