d24f21341e343a5d6597268e42de385cd010ff62
Jonathan Heugh <mrjbheugh@gmail.com>
2021-03-12 15:25:18 +0200

    Worked on alarm project

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,35 @@
-function setAlarm() {}
+const timeLimit = 20;
+let timePassed = 0;
+let timeLeft = timeLimit;
+let timerInterval = null;
+
+const timesUp = () => {
+  clearInterval(timerInterval);
+  playAlarm();
+}
+
+
+const setAlarm = () => {
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    timeLeft = timeLimit - timePassed;
+    document.getElementById('timeRemaining').innerHTML = `Time Remaining: ${formatTime(timeLeft)}`;
+    if (timeLeft === 0) {
+      timesUp();
+    }
+  }, 1000);
+}
+
+const formatTime = (time) => {
+  let minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  if(seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  return `${minutes}:${seconds}`;
+}
 
 // DO NOT EDIT BELOW HERE
 
