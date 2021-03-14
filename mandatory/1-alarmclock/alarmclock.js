f5319d4608ba29707d23988bf84fdc260ebf13e7
JarrodBen <46967203+JarrodBen@users.noreply.github.com>
2021-03-14 12:46:01 +0200

    Progress with AlarmClock

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,20 @@
-function setAlarm() {}
+let pageBody = getElementById("body")
+
+function setAlarm() {
+  let setAlarm = document.getElementById("alarmSet")
+  let setAlarmTime = document.getElementById("alarmSet").value
+  let remainTime = document.getElementById("timeRemaining")
+  let alarmTimeSet = document.getElementById("set")
+  let alarmTimeResume = document.getElementById("resume")
+  let alarmTimeStop = document.getElementById("stop")
+
+  let alarmSeconds = setAlarmTime % 60
+  let alarmMinutes = Math.floor(setAlarmTime % 60)
+
+  remainTime.textContent = `Time Remaining : ${alarmMinutes} : ${alarmSeconds}`
+
+  let 
+}
 
 // DO NOT EDIT BELOW HERE
 
