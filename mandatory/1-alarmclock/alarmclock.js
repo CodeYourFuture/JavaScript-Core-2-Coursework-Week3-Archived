91da4580c2b5a21b1d1df80f4f8b7bf51d602b5b
BenyamWorku <benworku@gmail.com>
2021-03-15 18:54:01 +0000

    alarm begin

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,37 @@
-function setAlarm() {}
+function setAlarm() {
+  let tRemainEl = document.
+    getElementById("timeRemaining");
+  let inputEl = document.
+    getElementById("alarmSet");
+  // console.log(tRemainEl.TEXT_NODE);
+  // console.log(tRemainEl.textContent);
+  let displayedTime = inputEl.value;
+  let timer;
+  // find out how to run just one timer
+  if (!timer) {
+   timer = setInterval(() => {
+    // console.log(`displayed time is ${displayedTime} on line 10`);
+    displayedTime--;
+    tRemainEl.textContent = `Time Remaining:${displayedTime}`;
+    // console.log(`displayed time is ${displayedTime} on line 12`);
+    if (displayedTime === 0) {
+      var audio = new Audio("alarmsound.mp3");
+      audio.play();
+      clearInterval(timer);
+    }
+    // inputEl.value="";
+   }, 1000);
+  }
+  
+  
+  let stopEl = document.getElementById("stop");
+  stopEl.addEventListener("click", () => {
+    clearInterval(timer);
+    tRemainEl.textContent = "Time Remaining:00:00";
+  })
+
+}
+
 
 // DO NOT EDIT BELOW HERE
 
