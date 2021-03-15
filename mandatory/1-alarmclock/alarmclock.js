a53db68d67f675276839047ba445454223631fae
Kudah Shambare <Kudahshambare263@gmail.com>
2021-03-15 13:16:28 +0200

    Alarm Done

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,27 @@
-function setAlarm() {}
+function setAlarm() {
+  /*
+. When the `Set Alarm` button is clicked, get the value of the input field
+2. When you have the input field value, set the title to the correct value
+3. Work out how to update the `Time Remaining` title every second
+4. When the remaining time left is 0, play the alarm sound
+
+  */
+  let setAlarmButton = document.getElementById("set");
+  let userTime = document.getElementById("alarmSet");
+  let remainingTime = document.getElementById("timeRemaining");
+  setAlarmButton.addEventListener("click", function () {
+    setInterval(function () {
+      if (userTime.value > 0) {
+        userTime.value--;
+        remainingTime.innerHTML = "Time Remaining: 00:" + userTime.value;
+      } else {
+        remainingTime.innerHTML = "Time Remaining: 00:00";
+        playAlarm();
+        userTime.reset();
+      }
+    }, 1000);
+  });
+}
 
 // DO NOT EDIT BELOW HERE
 
