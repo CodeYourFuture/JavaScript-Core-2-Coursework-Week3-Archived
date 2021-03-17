6ccf3e18b662e314501dde187742a0913a031cde
BenyamWorku <benworku@gmail.com>
2021-03-17 22:17:03 +0000

    slider half done

--- mandatory/3-slideshow/slideshow.js
+++ mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,28 @@
 // Write your code here
+let imgEl = document.querySelectorAll("img");
+let btnEl = document.querySelectorAll("button");
+let currentIndex = 0;
+let lengthImagesEl=imgEl.length;
+function showSlide() {
+    return imgEl[currentIndex].classList.add("showing");
+}
+
+function nextSlide() {
+    imgEl[currentIndex].style.display="none";
+    currentIndex = (currentIndex + 1) % lengthImagesEl;
+    return showSlide(currentIndex);
+
+}
+
+function previousSlide() {
+    imgEl[currentIndex].style.display = "none";
+    currentIndex = (currentIndex + lengthImagesEl - 1) % lengthImagesEl;
+    return showSlide(currentIndex);
+ }
+
+btnEl[0].addEventListener("click", previousSlide);
+
+btnEl[1].addEventListener("click", nextSlide);
+    
+
+
