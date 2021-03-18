4aa0aba209bdf46bfb1c12b9c874d8d016751132
BenyamWorku <benworku@gmail.com>
2021-03-18 21:59:11 +0000

    comments deleted

--- mandatory/3-slideshow/slideshow.js
+++ mandatory/3-slideshow/slideshow.js
@@ -1,13 +1,10 @@
-// Write your code here
 let imgEl = document.querySelectorAll("img");
 let backBtnEl = document.querySelector(".back-btn");
 let forwardBtnEl = document.querySelector(".forward-btn");
 let currentIndex = 0;
-let lengthImagesEl=imgEl.length;
+let lengthImagesEl = imgEl.length;
 function showSlide(currentIndex) {
-    let temp = currentIndex;
-    console.log(currentIndex);
-    return imgEl[temp].classList.add("showing");
+    return imgEl[currentIndex].classList.add("showing");
 }
 
 function nextSlide() {
@@ -20,14 +17,13 @@ function nextSlide() {
 function previousSlide() {
     imgEl[currentIndex].classList.toggle("showing");
 
-    // imgEl[currentIndex].style.display = "none";
     currentIndex = (currentIndex + lengthImagesEl - 1) % lengthImagesEl;
     return showSlide(currentIndex);
- }
+}
 
 backBtnEl.addEventListener("click", previousSlide);
- 
+
 forwardBtnEl.addEventListener("click", nextSlide);
-    
+
 
 
