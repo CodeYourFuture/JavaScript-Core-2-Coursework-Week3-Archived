ce073d0d702334ca2e0acad907bb56a95be88b1d
Kara <kara.mh191@gmail.com>
2021-03-19 22:01:32 +0000

    DOM Practice - Complete
    DOM Practice - Complete

--- InClass/DOM-practice/main.js
+++ InClass/DOM-practice/main.js
@@ -4,10 +4,10 @@ console.log("Testing JS file loaded!")
 
 // Without changing any of the HTML or CSS, update the <section> tags so that they have white backgrounds.
 
-
-
-
-
+let sectionElements = document.querySelectorAll("section");
+sectionElements.forEach((section) => {
+    section.style.backgroundColor = "white";
+});
 
 // Task 2
 
@@ -15,11 +15,37 @@ console.log("Testing JS file loaded!")
 
 // Hint: look at the CSS to see if there are any classes already written which you can use.
 
-
-
-
-
+let imageElements = document.querySelectorAll("img");
+imageElements.forEach((imageEl) => {
+    imageEl.className = "content-title";
+});
 
 // Task 3
 
 // Google the date of birth and death of each of the people on the page. Without changing any of the HTML or CSS, add this in a paragraph to the end of their <section>.
+
+const mothersOfComputing = [
+    {
+        name: "Grace Hopper",
+        dateOfBirth: "9 December 1906",
+        dateOfDeath: "1 January 1992"
+    },
+    {
+        name: "Katherine Johnson",
+        dateOfBirth: "26 August 1918",
+        dateOfDeath: "24 February 2020"
+    },
+    {
+        name: "Ada Lovelace",
+        dateOfBirth: "10 December 1815",
+        dateOfDeath: "27 November 1852"
+    }
+];
+
+sectionElements.forEach((section) => {
+    let paragraphElement = document.createElement("p");
+    mothersOfComputing.forEach((person) => {
+        paragraphElement.innerText = `D.O.B: ${person.dateOfBirth} \n D.O.D: ${person.dateOfDeath}`;
+        section.appendChild(paragraphElement);
+    });
+});
