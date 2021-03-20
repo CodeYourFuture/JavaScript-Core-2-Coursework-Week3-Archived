a40e037219f844fb3f0a4cb2029ed7a45ada3f41
unknown <ademalagoz46@gmail.com>
2021-03-20 01:29:57 +0000

    alarm clock

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,30 @@
-function setAlarm() {}
+function setAlarm() {
+  let timeRemain = document.getElementById('timeRemaining');
+  let setTime = document.getElementById('alarmSet').value;
+
+  let timer = setInterval(timing,1000);
+
+  function timing (){
+
+    if(setTime < 0){
+      playAlarm();
+      let setAlarmColor = document.querySelector('body');
+      setAlarmColor.style.backgroundColor = 'red';
+      clearInterval(timer);
+      
+    }//if closing
+
+    else {
+      let minutes = Math.floor(setTime / 60);
+      let seconds = setTime % 60;
+      timeRemain.innerText = `Time Remaining: ${minutes}:${seconds}`;
+      setTime -= 1;
+      let setAlarmColor = document.querySelector('body');
+      setAlarmColor.style.backgroundColor = 'green';
+    }
+
+  }//timing func closing
+}
 
 // DO NOT EDIT BELOW HERE
 
