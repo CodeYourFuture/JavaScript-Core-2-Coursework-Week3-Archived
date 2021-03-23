d68cdedae5cc3a3d6180b401c6790806c6ae4bf6
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-03-23 21:28:40 +0200

    Add image folder, set HTML strcuture

--- mandatory/3-slideshow/slideshow.js
+++ mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,12 @@
 // Write your code here
+//Photo by <a href="https://unsplash.com/@hilltopjr?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">deshawn wilson</a> on <a href="/t/nature?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+//Photo by <a href="https://unsplash.com/@huchenme?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Hu Chen</a> on <a href="/t/nature?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+//Photo by <a href="https://unsplash.com/@damianpatkowski?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Damian Patkowski</a> on <a href="/t/nature?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+//Photo by <a href="https://unsplash.com/@hitobywong?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Toby Wong</a> on <a href="/t/nature?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+//Photo by <a href="https://unsplash.com/@ringulreid?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Gulnaz Shaidullina</a> on <a href="/t/nature?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+//Photo by <a href="https://unsplash.com/@rfrsrh?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Foad Roshan</a> on <a href="/t/nature?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+//Photo by <a href="https://unsplash.com/@jacobdyer?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jacob Dyer</a> on <a href="/t/nature?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+//Photo by <a href="https://unsplash.com/@theoeilertsen?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Theo Eilertsen Photography</a> on <a href="/t/nature?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+let image = document.getElementById("img");
+let backBtn = document.getElementById("back");
+let fwdBtn = document.getElementById("fwd");
