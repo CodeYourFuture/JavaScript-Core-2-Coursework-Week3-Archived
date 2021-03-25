54353fba1896100aeadcc49b7fe162a0dc69d6b8
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-03-25 23:03:47 +0200

    Update alarmclock.js
    Focus on what is returned and not what the user sees.
    Access the user input and do something with it.

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,40 +1,59 @@
+let alarmTimer = 0;
+
+let alarmInterval = 0;
+
+function getStringTime(secondsInput) {
+  let minutes = Math.trunc(secondsInput / 60);   // converting seconds to minutes.
+  let seconds = secondsInput % 60;   //remainder(%) of the seconds.
+  return ` ${minutes}:${seconds.toString().padStart(2, "0")}`;
+}
+
+function myTimer() {
+  alarmTimer--;
+// console.log(alarmTimer);
+if (alarmTimer === 0) {
+  playAlarm();
+  clearInterval(alarmInterval);
+  }
+const titleVal = document.getElementById('timeRemaining');
+// titleVal.placeholder = '0:00';  //.toFixed(2) two decimal places.
+titleVal.textContent = `Time remaining: ${getStringTime(alarmTimer)}`;
+// const date = new Date();
+// inputVal.innerHTML.getTime();
+// console.log('This is the current time', time);
+}
+
 function setAlarm() {
   
-  const setBtn = document.getElementById('set');
-  // console.log('this is the set button', setBtn);
+  // const setBtn = document.getElementById('set');
+  // // console.log('this is the set button', setBtn);
   
-  const stopBtn = document.getElementById('stop');
-  // console.log('this is the stop button', stopBtn);
+  // const stopBtn = document.getElementById('stop');
+  // // console.log('this is the stop button', stopBtn);
+ 
   
-  setBtn.addEventListener('click', getInputValue)
-      function getInputValue(){
-        const inputVal = document.getElementById('alarmSet').value;
-        // Select the input element and get its value 
-        // console.log('This is my input value',inputVal);
-      
+    const inputVal = document.getElementById('alarmSet').value;
+    // Select the input element and get its value 
+    // console.log('This is my input value',inputVal);
+    alarmTimer = parseInt(inputVal * 60);
+    const titleVal = document.getElementById('timeRemaining');
+    // titleVal.placeholder = '0:00';  //.toFixed(2) two decimal places.
+    titleVal.textContent = `Time remaining: ${getStringTime(alarmTimer)}`;
         
-        const titleVal = document.getElementById('timeRemaining');
-        titleVal.placeholder = '0:00';  //.toFixed(2) two decimal places.
-        titleVal.textContent = `Time remaining: ${inputVal}`;
-        // console.log('This is my titleVal', titleVal);
-    }
-  
-  //---------set interval------------//
-  // const alarm = setInterval(myTimer, 1000);
-
-
-  // function myTimer() {
-  //   const date = new Date();
-  //   const time = date.toLocaleTimeString();    
-  //   document.getElementsByTagName('input').innerHTML = time;
-  //   console.log('This is the current time', time);
-  // }
-  // //---------stop interval------------//
-  // function stop() {
-  //   clearInterval(alarm);
-  // }
+    // console.log('This is my titleVal', titleVal);
+    //---------set interval------------//
+  alarmInterval = setInterval(myTimer, 1000);
+  console.log(alarmInterval);                
 }
-setAlarm();
+
+//---------stop interval------------//
+// function pauseAlarm() {
+//   clearInterval(alarmInterval);
+//   console.log('stopping alarm');
+// }
+
+
+
 
 //---------------------//
 
@@ -46,7 +65,7 @@ function setup() {
   document.getElementById("set").addEventListener("click", () => {
     setAlarm();
   });
-
+  
   document.getElementById("stop").addEventListener("click", () => {
     pauseAlarm();
   });
