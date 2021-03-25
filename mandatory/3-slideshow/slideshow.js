ee8c96baa8a19a4eaca65eabc6d7ace31f6e40c2
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-03-25 11:57:05 +0200

    Complete HTML for slide-show

--- mandatory/3-slideshow/slideshow.js
+++ mandatory/3-slideshow/slideshow.js
@@ -1,12 +1,24 @@
 // Write your code here
-//Photo by <a href="https://unsplash.com/@hilltopjr?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">deshawn wilson</a> on <a href="/t/nature?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-//Photo by <a href="https://unsplash.com/@huchenme?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Hu Chen</a> on <a href="/t/nature?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-//Photo by <a href="https://unsplash.com/@damianpatkowski?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Damian Patkowski</a> on <a href="/t/nature?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-//Photo by <a href="https://unsplash.com/@hitobywong?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Toby Wong</a> on <a href="/t/nature?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-//Photo by <a href="https://unsplash.com/@ringulreid?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Gulnaz Shaidullina</a> on <a href="/t/nature?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-//Photo by <a href="https://unsplash.com/@rfrsrh?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Foad Roshan</a> on <a href="/t/nature?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-//Photo by <a href="https://unsplash.com/@jacobdyer?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jacob Dyer</a> on <a href="/t/nature?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-//Photo by <a href="https://unsplash.com/@theoeilertsen?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Theo Eilertsen Photography</a> on <a href="/t/nature?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-let image = document.getElementById("img");
+let image1 = document.getElementById("img1");
+let image2 = document.getElementById("img2");
+let image3 = document.getElementById("img3");
+let image4 = document.getElementById("img4");
+let image5 = document.getElementById("img5");
+let image6 = document.getElementById("img6");
+let image7 = document.getElementById("img7");
+let image8 = document.getElementById("img8");
+
 let backBtn = document.getElementById("back");
 let fwdBtn = document.getElementById("fwd");
+
+let imgArray = [image1, image2, image3, image4, image5, image6, image7, image8];
+
+window.addEventListener("load", ()=>{
+    imgArray[0].style.display = "block";
+});
+
+function slideShow (imageArr){
+    if(indexOf(imageArr) >= 0){
+
+    }
+}
