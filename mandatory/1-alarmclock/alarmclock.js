20a52bc08a4b4313f5218f594750502d84e27ebd
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-03-29 20:53:36 +0200

    Edit conditionals

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,24 +1,30 @@
 function setAlarm() {
   let alarmInput = document.getElementById("alarmSet");
   let remainingTime = document.getElementById("timeRemaining");
+
   let timer = setInterval(() => {
-    if (alarmInput.value >= 0 < 10){
+    if (alarmInput.value === 0){
+      remainingTime.innerHTML = "Time Remaining: 00:00";
+      alarmInput.value = "";
+      playAlarm();
+    } else if (alarmInput.value >= 0 < 10){
     remainingTime.innerHTML = `Time Remaining: 00:0${alarmInput.value}`;
     alarmInput.value -= 1;
     }else if (alarmInput.value >=10 <100){
       remainingTime.innerHTML = `Time Remaining: 00:${alarmInput.value}`;
-      // alarmInput.value -= 1;
-    }else if(alarmInput.value >100 < 1000){
+      alarmInput.value -= 1;
+    }else if(alarmInput.value >=100 < 1000){
       remainingTime.innerHTML = `Time Remaining: 0${alarmInput.value}:00`;
-      // alarmInput.value -= 1;
-    }else if (alarmInput.value === 0){
-      remainingTime.innerHTML = "Time Remaining: 00:00";
-      alarmInput.value = "";
-      playAlarm();
+      alarmInput.value -= 1;
     }
     
   }, 1000);
-  document.getElementById("stop").addEventListener("click", () => clearInterval(timer));
+
+  document.getElementById("stop").addEventListener("click", () => {
+    clearInterval(timer);
+    alarmInput.value = "";
+  });
+
 }
 
 
