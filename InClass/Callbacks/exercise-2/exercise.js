92bf7facc46575a3b70a2d43d0f0f652a53c7867
musayuksel <musayuxel@gmail.com>
2021-08-02 00:15:01 +0100

    Add InClass exercises

--- InClass/Callbacks/exercise-2/exercise.js
+++ InClass/Callbacks/exercise-2/exercise.js
@@ -1,4 +1,4 @@
-/*
+/* 
 ================
 Exercise 2
 ----------
@@ -9,7 +9,19 @@ Create a function called "showMovies" that
 - iterates through the "movies" array and
 - for each movie, it creates a <p> element with the movie title and director and append it to the #all-movies div.
 - it sets the innerText of the #movies-number element to the total number of the movies in the array "movies"
+*/
+// function showMovies(movies) {
+//   const allMovies = document.querySelector('#all-movies');
+//   movies.forEach(movie => {
+//     const p = document.createElement('p');
+//     p.innerText = `${movie.title}  by ${movie.director}`;
+//     allMovies.appendChild(p);
+//   });
+//   const movieNumber = document.querySelector('#movies-number');
+//   movieNumber.innerText = movies.length;
+// }
 
+/* 
 Task 2
 Amend your function above to only show movies after 1 second. Remember to use setTimeout to achieve that
 Create a new function called "addMovie"
@@ -17,11 +29,40 @@ Create a new function called "addMovie"
 - it adds the new movie to the list of movies after 2 seconds. Remember to setTimeout to achieve that
 Call addMovies to add the new movie to the list and then showMovies to see the movies added on the screen.
 How many movies can you see on your page?
+*/
+function addMovie(obj,callBack){
+  console.log('waiting 2 secs FOR ADDING movies<<<<<')
+  setTimeout(()=>{
+    movies.push(obj);// add new film object >> to movie array 
+    callBack(movies);// show new array
+  },1000);
+}
 
+
+function showMovies(movies) {
+  console.log('waiting 1 sec FOR SHOWING movies<<<<<')
+  setTimeout(() =>{
+    document.querySelectorAll("#all-movies p:not(.alert)").forEach(e => e.parentNode.removeChild(e));//choose all movie in the page and delete them  ****!!!! NOTE for MATT>>>> please look at END OF THE PAGE
+    const allMovies = document.querySelector('#all-movies');
+    movies.forEach(movie => {
+      const p = document.createElement('p');
+      let film = `${movie.title}  by ${movie.director}`;
+      p.innerText = film;
+      allMovies.appendChild(p);
+    });
+    
+    const movieNumber = document.querySelector('#movies-number');
+    movieNumber.innerText = movies.length;
+  },500);
+}
+
+
+/* 
 Task 3
 Can you make sure the new movie you just added is showing on the screen? 
 TIP: use callbacks
-
+*/
+/* 
 Task 4 - **Extra**
 Create a form anywhere on your page. The form should have
 - 4 input text fields, one for each property of your movie object
@@ -34,6 +75,18 @@ TIP: Use the functions you created on tasks 1-3
 Prefer to work on a codepen? https://codepen.io/makanti/pen/MWwMgmW?editors
 ================
 */
+const submit = document.getElementById('submit');
+submit.addEventListener('click',()=>{
+  const newFilm ={
+  title: document.getElementById('title').value,
+  director : document.getElementById('director').value, 
+  type : document.getElementById('type').value,
+  haveWatched : document.getElementById('haveWatched').value
+  };
+  addMovie(newFilm,showMovies);
+})
+
+
 const movies = [
   {
     title: "Color Out of Space",
@@ -61,8 +114,32 @@ const movies = [
   },
 ];
 
-// create showMovies function
 
+showMovies(movies);
 // create a new movie object for your favorite movie
+const newmovie ={
+  title: "CYF",
+  director: "Richard Stanley",
+  type: "TEC",
+  haveWatched: true,
+}
+addMovie(newmovie,showMovies);
 
-// create addMovies function
+
+
+/**
+ * 
+ * function isInHTML(film){
+  const allP =Array.from(document.querySelectorAll("#all-movies p:not(.alert)"))
+  console.log(allP);
+  return allP.some(p =>{
+    console.log(p.innerText,'<<<<<<<<<<<<<<<<PPPPP')
+     return p.innerText === film;
+  });
+}
+
+MATT in this function I tried to check HTML, if the film is already on the page, I don't need to add.. but i couldn't manage it. So I choose the easy way > when you call "showMovies" function, it will clear all old films and creating again.. 
+
+
+my main goal was to make all the changes to the object. then it was just changing the relevant places on the web page. otherwise, every function will have to make changes on the web page and I will be more likely to make mistakes. or webpage will download all data again 
+ */
--- InClass/DOM-practice/main.js
+++ InClass/DOM-practice/main.js
@@ -3,7 +3,10 @@ console.log("Testing JS file loaded!")
 // Task 1
 
 // Without changing any of the HTML or CSS, update the <section> tags so that they have white backgrounds.
-
+const sections =Array.from(document.querySelectorAll('section'));
+sections.forEach(section =>{
+  section.style.backgroundColor = 'white';
+})
 
 
 
@@ -14,8 +17,12 @@ console.log("Testing JS file loaded!")
 // Without changing any of the HTML or CSS, update the images on the page so that they are all centered.
 
 // Hint: look at the CSS to see if there are any classes already written which you can use.
-
-
+// .content-title
+const images = document.querySelectorAll('img');
+// console.log( images,'<<<<<<');
+images.forEach(img =>{
+  img.classList.add('content-title');
+});
 
 
 
@@ -23,3 +30,17 @@ console.log("Testing JS file loaded!")
 // Task 3
 
 // Google the date of birth and death of each of the people on the page. Without changing any of the HTML or CSS, add this in a paragraph to the end of their <section>.
+const p1 = `Born: 9 December 1906
+Died: 1 January 1992`;
+const p2 = `Born: 26 August 1918
+Died: 24 February 2020`;
+const p3 = `Born: 10 December 1815
+Died: 27 November 1852`;
+const bornAndDiedDates= [p1,p2,p3];
+
+sections.forEach((eachSection,index)=>{
+  //for every section > create p, put dates orderly
+  const p = document.createElement('p');
+  p.innerText =bornAndDiedDates[index];
+  eachSection.appendChild(p);
+});
