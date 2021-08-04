ae45431066c28c014c27431c2f416a13bbf6a664
maham511 <maha.malik511@gmail.com>
2021-08-04 11:20:28 +0100

    Html elements created. not getting random quotes/authors on button click

---
+++ codility.js
@@ -0,0 +1,7 @@
+// function printL (N) {
+//     for (let i = 0; i=N; i++){
+//         console.log('L');
+//     }
+// }
+
+// printL(4);
--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,25 +1,4 @@
 function setAlarm() {
-  // //stores input elem
-  // let inputField = document.getElementById("alarmSet");
-
-  // //stores h1 elem, adds span elem in h1 using .innerHTML
-  // let timeDisplay = document.querySelector("#timeRemaining");
-  // timeDisplay.innerHTML = "Time Remaining: 00:<span id = 'timeSpan'></span>";
-
-  // //store span elem
-  // let spanEl = document.querySelector("#timeSpan");
-  // spanEl.innerHTML = inputField.value;  //span renders input field value
-
-  // //setInterval reduces span's innerHTML value by 1 each second
-  // setInterval(function() {
-  //   spanEl.innerHTML--;
-  //   //when span's innerHTML renders as '0', call playAlarm function
-  //   if (spanEl.innerHTML === '0') {
-  //     playAlarm();
-  //   }
-  // }, 1000);
-
-  //--------------------------
 
   //stores input elem
   let inputField = document.getElementById("alarmSet");
@@ -45,13 +24,15 @@ function setAlarm() {
      secSpan.innerHTML--;
 
     //for min reverting to secs 
-    if (minSpan.innerHTML !== '00' && secSpan.innerHTML === '-1' ) {
-      minSpan.innerHTML --;
-      secSpan.innerHTML = '59'; 
-  }
-    // else if(minSpan.innerHTML === '0' && secSpan.innerHTML === '1' ) {
+    if (!minSpan.innerHTML === '0' && secSpan.innerHTML === '-1' ) {
+      minSpan.innerHTML--;
+      secSpan.innerHTML = "59";
 
-    // }
+    }
+    else if (minSpan.innerHTML ==='0' && secSpan.innerHTML === '-1'){
+      secSpan.innerHTML--
+    }
+   
 
     //when span's innerHTML renders as '0', call playAlarm function
    else if ((minSpan.innerHTML === "00" && secSpan.innerHTML === "0") || (minSpan.innerHTML === "0" && secSpan.innerHTML === '0')) {
@@ -59,21 +40,9 @@ function setAlarm() {
     }
   }, 1000);
 
-  // Turn values over 60 into mins
+  
 
-  //Tried adding 0 before numbers less than 10, but would render NAN as spanEl's innerHTML after timer goes into negative numbers:
-  // let startTimer = setInterval(function () {
-  //   // spanEl.innerHTML--;
-  //   if (spanEl.innerHTML < 11) {
-  //     spanEl.innerHTML = `0${spanEl.innerHTML - 1}`;
-  //   } else {
-  //     spanEl.innerHTML--;
-  //   }
 
-  //   if (spanEl.innerHTML === "00") {
-  //     playAlarm();
-  //   }
-  // }, 1000); //subtracts 1 from spanEl.innerHTML's value every second
 }
   
 
