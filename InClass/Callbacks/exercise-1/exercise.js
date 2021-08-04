c7c4461464dff7f5257db0cf7b844e895f077117
Maziarmajd <83129200+Maziarmajd@users.noreply.github.com>
2021-08-04 14:25:33 +0100

    in class 1

--- InClass/Callbacks/exercise-1/exercise.js
+++ InClass/Callbacks/exercise-1/exercise.js
@@ -11,3 +11,10 @@ Update your code to make the colour change every 5 seconds to something differen
 Prefer to work on a codepen? https://codepen.io/makanti/pen/abOreLg
 ================
 */
+//document.body.style.backgroundColor = "red";
+
+setInterval(
+function () {
+  var randomColor = Math.floor(Math.random()*16777215).toString(16);
+  document.body.style.backgroundColor = "#"+randomColor;
+},1000);
