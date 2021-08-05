b2c00ba2698495fb18136e6960a3d4db3668bb47
konikalily <konika.l.biswas@gmail.com>
2021-08-05 14:05:54 +0100

    callback exercises done

--- InClass/Callbacks/exercise-1/exercise.js
+++ InClass/Callbacks/exercise-1/exercise.js
@@ -11,3 +11,28 @@ Update your code to make the colour change every 5 seconds to something differen
 Prefer to work on a codepen? https://codepen.io/makanti/pen/abOreLg
 ================
 */
+//task 1
+
+function changeBgColor() {
+  document.body.style.backgroundColor = "lightblue";
+}
+setTimeout(changeBgColor, 5000);
+//task 2
+
+const bgColor = document.getElementById("main");
+
+const allColors = [
+  "green",
+  "orange",
+  "pink",
+  "lightblue",
+  "red",
+  "yellow",
+  "purple",
+];
+
+function changeBgColors() {
+  let randomColor = Math.floor(Math.random() * allColors.length); //choose a random color from allColors Array
+  bgColor.style.backgroundColor = allColors[randomColor];
+}
+setInterval(changeBgColors, 5000);
