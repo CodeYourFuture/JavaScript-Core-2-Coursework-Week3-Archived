42c7e7d8c788b50e21d0f208f6e6b6145c969b3e
konikalily <konika.l.biswas@gmail.com>
2021-08-05 19:55:33 +0100

    alarmclock edited

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,39 +1,24 @@
 function setAlarm() {
   const timeRemaining = document.getElementById("timeRemaining");
-  const timeInputValue = document.getElementById("alarmSet").value;
-  const setAlarmBtn = document.getElementById("set");
-  let stopAlarmBtn = document.getElementById("stop");
-
-  let timeCount = timeInputValue,
-    clear;
-
-  convertTime();
-  setAlarmBtn.addEventListener("click", setAlarmTime);
-  stopAlarmBtn.addEventListener("click", stopTimeCount);
+  let timeInputValue = document.getElementById("alarmSet").value;
 
   function convertTime() {
-    const timesInMinutes = Math.floor(timeCount / 60);
-    const timesInSeconds = timeCount % 60;
-    timeCount--;
+    const timesInMinutes = Math.floor(timeInputValue / 60);
+    const timesInSeconds = timeInputValue % 60;
+    if (timeInputValue === 0) {
+      playAlarm();
+      clearInterval(clear);
+      //stopTimeCount();
+      timeRemaining.innerText = "Time Remaining: 00:00";
+    } else {
+      timeInputValue--;
+    }
     timeRemaining.innerText = `Time Remaining: ${timesInMinutes}:${timesInSeconds}`;
   }
-  function setAlarmTime() {
-    clear = setInterval(convertTime, 1000);
-    if (timeCount < 0) {
-      playAlarm();
-      stopTimeCount();
-      timeRemaining.innerText = "Time Remaining: 00:00";
-    }
-  }
-  function stopTimeCount() {
-    clearInterval(clear);
-  }
+
+  let clear = setInterval(convertTime, 1000);
 }
 
-//  When the `Set Alarm` button is clicked, get the value of the input field
-// 2. When you have the input field value, set the title to the correct value
-// 3. Work out how to update the `Time Remaining` title every second
-// 4. When the remaining time left is 0, play the alarm sound
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
