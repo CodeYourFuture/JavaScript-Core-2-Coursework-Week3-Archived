8f74a9e0c169d3bad0e56b31e1ccc8a513b3173d
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-08-06 20:47:49 +0100

    slideshow attempted

--- mandatory/3-slideshow/slideshow.js
+++ mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,50 @@
-// Write your code here
+const forward = document.getElementById('forward');
+const back = document.getElementById('back');
+const slide = document.getElementById('firstImage');
+
+
+forward.addEventListener('click', ()=>{
+  
+});
+
+const images = [
+    'https://images.unsplash.com/photo-1451186859696-371d9477be93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2852&q=80',
+    
+    'https://images.unsplash.com/photo-1483519173755-be893fab1f46?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2883&q=80',
+    
+    'https://images.unsplash.com/photo-1542601906990-b4d3fb778b09?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2948&q=80',
+        
+    'https://images.unsplash.com/photo-1484553255294-313b931acd27?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1652&q=80',
+    
+    'https://images.unsplash.com/photo-1537086838011-7e410bee78b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1629&q=80',
+    
+    'https://images.unsplash.com/photo-1464151759330-a3441f3da55e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80'
+        
+]
+
+// const images = [
+//     {
+//         src: 'https://images.unsplash.com/photo-1451186859696-371d9477be93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2852&q=80',
+//         alt: 'ocean'
+//     },
+//     {
+//         src: 'https://images.unsplash.com/photo-1483519173755-be893fab1f46?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2883&q=80',
+//         alt: 'eye'
+//     },
+//     {
+//         src: 'https://images.unsplash.com/photo-1542601906990-b4d3fb778b09?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2948&q=80',
+//         alt: 'plant-in-the-palm'
+//     },
+//     {
+//         src: 'https://images.unsplash.com/photo-1484553255294-313b931acd27?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1652&q=80',
+//         alt: 'succulents'
+//     },
+//     {
+//         src: 'https://images.unsplash.com/photo-1537086838011-7e410bee78b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1629&q=80',
+//         alt: 'gate with plants'
+//     },
+//     {
+//         src: 'https://images.unsplash.com/photo-1464151759330-a3441f3da55e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80',
+//         alt: 'flamingoes'
+//     }
+// ]
