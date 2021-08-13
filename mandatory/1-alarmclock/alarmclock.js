818bc622487bfb381ac4d39c3afe1582293147b6
Ivan Zhukov <zhkvivan@gmail.com>
2021-08-13 08:33:54 +0100

    quotes done

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,51 @@
-function setAlarm() {}
+function setAlarm() {
+	let timeInSec = document.querySelector("#alarmSet").value;
+	let min, sec;
+
+	if (timeInSec > 60) {
+		min = Math.floor(timeInSec / 60);
+		sec = timeInSec - 60 * min;
+		// function withZero(n) {
+		// 	return "0" + n;
+		// }
+		// if (min <= 9) { min = withZero(min) };
+		// if (sec <= 9) { sec = withZero(sec) };
+	} else {
+		min = 0;
+		sec = timeInSec;
+	// 	function withZero(n) {
+    //   return "0" + n;
+    // }
+	// 	if (sec <= 9) { sec = withZero(sec) };
+	}
+
+	console.log(`${min} min and ${sec} sec`);
+
+	let timeRemaining = document.querySelector("#timeRemaining");
+	
+
+	let timer = setInterval(function(){
+		if (sec < 10) {
+			sec = '0' + sec;
+		} 
+		if (min < 10) {
+			min = '0' + min;
+		}
+		timeRemaining.textContent = `Time Remaining: ${min}:${sec}`;
+		sec = sec - 1;
+		
+		if (timeRemaining.textContent === `Time Remaining: 00:00`) {
+			clearInterval(timer);
+		}
+	}, 1000)
+	
+	
+	
+}
+
+let time = document.querySelector("#alarmSet");
+
+
 
 // DO NOT EDIT BELOW HERE
 
