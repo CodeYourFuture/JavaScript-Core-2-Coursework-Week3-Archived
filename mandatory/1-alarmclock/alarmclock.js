a4a51fc98bf776d0b08250338892692b5ddacb65
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2021-08-16 06:15:13 +0100

    update alarm clock

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,34 @@
-function setAlarm() {}
+function setAlarm() {
+  const timeRemaining = document.getElementById("timeRemaining");
+  const alarmSet = document.getElementById("alarmSet");
+  const setAlarmBtn = document.getElementById("set");
+  const stopAlarmBtn = document.getElementById("stop");
+  const pauseAlarmBtn = document.getElementById("pause");
+
+
+  let count;
+  setAlarmBtn.addEventListener("click", function(){
+    count = alarmSet.value;
+
+    const intervalId = setInterval(function(){
+      let minutes = Math.floor(count / 60);
+      let seconds = count % 60;
+      
+      timeRemaining.innerHTML = `Time Remaining: ${minutes}:${seconds}`;
+      if (count == 0){
+        audio.play();
+        clearInterval(intervalId);
+        document.body.style.backgroundColor = "orange";
+      }
+
+      count --;
+    }, 1000);
+  });
+
+  pauseAlarmBtn.addEventListener("click", function(){
+    setAlarmBtn.pause();
+  });
+}
 
 // DO NOT EDIT BELOW HERE
 
