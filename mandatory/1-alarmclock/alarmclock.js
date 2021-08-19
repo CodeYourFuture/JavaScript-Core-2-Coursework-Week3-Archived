293945bc7a7d4b818a48d32562fd1d34a1bd7e8c
Sharm-Dev <sharmainetaylor@ymail.com>
2021-08-19 01:07:55 +0100

    Completed outsatanding work

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,59 @@
-function setAlarm() {}
+function setAlarm() {
+  
+  let isAlarmSet = document.getElementById("alarmSet").value;
+  const isTimeRemaining = document.getElementById("timeRemaining")
+
+  const isInterval = setInterval(() => {
+    const minutes = Math.floor(isAlarmSet / 60).toString().padStart(2, '0')
+    const seconds = (isAlarmSet % 60).toString().padStart(2, '0')
+    isTimeRemaining.innerText = `Time Remaining: ${minutes}:${seconds}`
+    if(isAlarmSet === 0){
+      clearInterval(isInterval)
+    } else {
+      isAlarmSet -- 
+      console.log(isAlarmSet)
+    }
+
+    const alarmSound = alarmPlaying(()=>{
+      const playButton =document.getElementById("button")
+    if(isAlarmSet === 0){
+      alarmPlaying(audio.play)
+    }
+
+  })
+
+    
+
+  },1000)
+
+  
+
+
+
+    
+
+  
+ 
+  // const minutes = isAlarmSet / 60 
+  // const hours = isAlarmSet + 4;
+  // const timeDisplayed = `${hours} + ':' + ${minutes}`
+
+  // isTimeRemaining.appendChild(timeDisplayed)
+ 
+
+
+
+
+
+
+
+
+
+  
+
+
+
+}
 
 // DO NOT EDIT BELOW HERE
 
