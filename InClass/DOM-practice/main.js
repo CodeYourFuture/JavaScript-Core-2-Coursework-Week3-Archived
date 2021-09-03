9ad4b9e5689f8331040303243711b996a0aa9292
Niamh_Madden <Niamhmadden15@gmail.com>
2021-09-03 20:10:51 +0100

    unfinished quote generator

--- InClass/DOM-practice/main.js
+++ InClass/DOM-practice/main.js
@@ -1,24 +1,26 @@
-console.log("Testing JS file loaded!")
+console.log("Testing JS file loaded!");
 
 // Task 1
 
 // Without changing any of the HTML or CSS, update the <section> tags so that they have white backgrounds.
+const section = document.getElementsByTagName("section");
+// console.log(section);
 
-
-
-
-
+Array.from(section).forEach((el) => {
+  el.style.backgroundColor = "#fff";
+});
 
 // Task 2
 
 // Without changing any of the HTML or CSS, update the images on the page so that they are all centered.
 
+const images = document.querySelectorAll("img");
+images.forEach((img) => {
+  img.style.display = "block";
+  img.style.marginLeft = "auto";
+  img.style.marginRight = "auto";
+});
 // Hint: look at the CSS to see if there are any classes already written which you can use.
-
-
-
-
-
 
 // Task 3
 
