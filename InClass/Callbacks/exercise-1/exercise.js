2d2891db1ba8fda7d00d447b0f004698f5895067
Megumi Yanaka <margaret_mori0102@yahoo.com>
2021-09-29 22:50:44 +0100

    finished call back>exercise-1

--- InClass/Callbacks/exercise-1/exercise.js
+++ InClass/Callbacks/exercise-1/exercise.js
@@ -11,3 +11,24 @@ Update your code to make the colour change every 5 seconds to something differen
 Prefer to work on a codepen? https://codepen.io/makanti/pen/abOreLg
 ================
 */
+
+// Task 1
+function changeBackgroundColor(){
+   document.body.style.backgroundColor = "green";
+}
+setTimeout(changeBackgroundColor,5000);
+
+// Task2
+let i = 0 ; 
+color = new Array("red","green","blue","yellow","purple","gray","white") ;
+function change(){
+i++ ;
+if(i >= color.length) {
+i = 0 ;
+}
+document.body.style.backgroundColor = color[i];
+}
+function continueChangeBackground(){
+setInterval("change()",1000) ;
+}
+window.onload = continueChangeBackground() ;
