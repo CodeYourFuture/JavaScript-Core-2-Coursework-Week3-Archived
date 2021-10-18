434d35be35c823926ffbba7034b8b2d474f0dca7
tenzyns <86624297+tenzyns@users.noreply.github.com>
2021-10-18 19:34:34 +0100

    Alarm timer completed

--- InClass/Callbacks/exercise-1/exercise.js
+++ InClass/Callbacks/exercise-1/exercise.js
@@ -11,3 +11,14 @@ Update your code to make the colour change every 5 seconds to something differen
 Prefer to work on a codepen? https://codepen.io/makanti/pen/abOreLg
 ================
 */
+//setTimeout(changeBackground, 5000);
+
+function changeBackground(){
+   document.body.style.backgroundColor = "green";
+}
+
+let colours = ["green", "red", " white", "yellow"];
+setInterval(() => {
+    let randomIndex = Math.floor(Math.random() * colours.length)
+    document.body.style.backgroundColor = colours[randomIndex];
+}, 2000);
