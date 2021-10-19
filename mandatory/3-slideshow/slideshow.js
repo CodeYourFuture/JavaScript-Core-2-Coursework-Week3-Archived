62533cb71e6e4ad22141e3bc15e5c1d58c6d9185
dotdashdon <80584408+dotdashdon@users.noreply.github.com>
2021-10-19 14:05:43 +0100

    added images

--- mandatory/3-slideshow/slideshow.js
+++ mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,21 @@
-// Write your code here
+
+var  i =0; //start point 
+let images = [];// Write your code here
+let time = 1000;
+
+//Image List 
+images[0] = 'image1.jpg';
+images[1] = 'image2.jpg';
+images[2] = 'image3.jpg';
+images[3] = 'image3.jpg';
+images[4] = 'image3.jpg';
+
+
+//change image 
+
+function changeImg(){
+    document.slide.src =images[1];
+}
+
+
+//let image = document.getElementById("image");
