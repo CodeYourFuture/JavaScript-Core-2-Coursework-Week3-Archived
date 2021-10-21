03433f4b44cdff05928995b176abf356e3139230
Peter Ahern <peterahern87@gmail.com>
2021-10-21 10:04:37 +0100

    Completed Alarm Clock exercise
    Alarm Clock exercise complete, but with some functionality issues.

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,28 @@
-function setAlarm() {}
+let timerSet = false;
+function setAlarm() {
+  let timeRemainingEl = document.getElementById("timeRemaining");
+  let userInput = document.getElementById("alarmSet");
+  let timeLeft = userInput.value;
+  timerSet = true;
+
+  function timer() {
+    timeLeft--;
+    if (timeLeft > 10) {
+      timeRemainingEl.innerHTML = `Time Remaining: 00:${timeLeft}`;
+    } else if (timeLeft < 10) {
+      timeRemainingEl.innerHTML = `Time Remaining: 00:0${timeLeft}`;
+    }
+  }
+  setInterval(() => {
+    if (timeLeft > 0) {
+      timer();
+    } else if (timerSet === true && timeLeft === 0) {
+      playAlarm();
+      timerSet = false;
+      userInput.value = "";
+    }
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
