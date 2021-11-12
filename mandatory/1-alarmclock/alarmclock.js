c42bd8a54609552b38e35737e756e7f92aa46b11
Nonye <nonyeagwunobi@gmail.com>
2021-11-12 22:15:21 +0000

    Mandatory tasks completed

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,25 +1,49 @@
-function setAlarm() {}
+function setAlarm() {
+    let counterTimeRemain = document.getElementById("timeRemaining");
+    let inputTime = document.getElementById("alarmSet").value;
+
+    let timeInMinutes = Math.floor(inputTime / 60);
+
+    let timeInSeconds = inputTime % 60;
+
+    let timeInterval = setInterval(() => {
+        if (timeInMinutes > 0 && timeInSeconds === 0) {
+            timeInMinutes--;
+            timeInSeconds = 59;
+        }
+        if (timeInSeconds === 0) {
+            clearInterval(timeInterval);
+            audio.play();
+            document.querySelector(".centre").style.backgroundColor = "red";
+        }
+
+        counterEl.textContent = `Time Remaining: ${timeInMinutes} : ${timeInSeconds}`;
+        timeInSeconds--;
+    }, 1000);
+
+    audio.pause();
+}
 
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
 
 function setup() {
-  document.getElementById("set").addEventListener("click", () => {
-    setAlarm();
-  });
+    document.getElementById("set").addEventListener("click", () => {
+        setAlarm();
+    });
 
-  document.getElementById("stop").addEventListener("click", () => {
-    pauseAlarm();
-  });
+    document.getElementById("stop").addEventListener("click", () => {
+        pauseAlarm();
+    });
 }
 
 function playAlarm() {
-  audio.play();
+    audio.play();
 }
 
 function pauseAlarm() {
-  audio.pause();
+    audio.pause();
 }
 
 window.onload = setup;
--- mandatory/3-slideshow/slideshow.js
+++ mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,55 @@
 // Write your code here
+const forwardBtn = document.getElementById("nextBtn");
+const backBtn = document.getElementById("backBtn");
+const picture = document.getElementById("image");
+const autoBack = document.getElementById("auto-back");
+const autoNext = document.getElementById("auto-next");
+const stopBtn = document.getElementById("stop");
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("image").src = picsArray[0];
+
+    let currentSlide = 0;
+    let timer = 5000;
+    let newBack;
+    let newNext;
+
+    function forwardSlides() {
+        if (currentSlide === picsArray.length - 1) {
+            currentSlide = 0;
+            picture.src = picsArray[0];
+        } else {
+            currentSlide++;
+            picture.src = picsArray[currentSlide];
+        }
+    }
+
+    function backwardSlides() {
+        if (currentSlide <= 0) {
+            picture.src = picsArray[picsArray.length - 1];
+            currentSlide = picsArray.length - 1;
+        }
+    }
+
+    nextBtn.addEventListener("click", forwardSlides);
+    backBtn.addEventListener("click", backwardSlides);
+
+    autoNext.addEventListener("click", () => {
+        newNext = setInterval(forwardSlides, timer);
+        clearInterval(() => {
+            newNext;
+        }, timer);
+    });
+
+    autoBack.addEventListener("click", () => {
+        newBack = setInterval(backwardSlides, timer);
+        clearInterval(() => {
+            newBack;
+        }, timer);
+    });
+
+    stopBtn.addEventListener("click", () => {
+        clearInterval(newNext);
+        clearInterval(newBack);
+    });
+});
